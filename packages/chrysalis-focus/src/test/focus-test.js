ba650ebf7d96c67e20596e72f68c854e9d1790b1
Gergely Nagy <algernon@keyboard.io>
2018-07-27 15:46:09 +0200

    Initial import
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ packages/chrysalis-focus/src/test/focus-test.js
@@ -0,0 +1,101 @@
+/* chrysalis-focus -- Chrysalis Focus protocol library
+ * Copyright (C) 2018  Gergely Nagy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import "chai/register-expect"
+
+import SerialPort from "serialport/test"
+import Focus from "../lib/chrysalis-focus"
+const MockBinding = SerialPort.Binding
+
+describe("Focus", () => {
+    let port, focus
+
+    function emitData(port, response) {
+        port.binding.emitData(Buffer.from(response + "\r\n.\r\n"))
+    }
+
+    beforeEach(() => {
+        MockBinding.createPort("/dev/ttyFake")
+
+        port = new SerialPort("/dev/ttyFake")
+        focus = new Focus(port)
+    })
+
+    afterEach(() => {
+        MockBinding.reset()
+    })
+
+    describe("#help", () => {
+        it ("parses the help correctly", (done) => {
+            emitData(port, "help\r\nversion")
+            focus.command("help").then((commands) => {
+                expect(commands).to.be.an("array").that.has.lengthOf(2)
+                expect(commands).to.have.ordered.members(["help", "version"])
+                done()
+            })
+        })
+    })
+
+    describe("#version", () => {
+        it("parses the version correctly", (done) => {
+            emitData(port, "Kaleidoscope/latest Fake/Device | today")
+            focus.command("version").then((version) => {
+                expect(version).to.be.a("object")
+                expect(version).to.deep.equal({
+                    board: {
+                        vendor: "Fake",
+                        product: "Device"
+                    },
+                    firmware: {
+                        name: "Kaleidoscope",
+                        version: "latest",
+                        date: "today"
+                    }})
+                done()
+            })
+        })
+    })
+
+
+    describe("#command", () => {
+        it("should return a single response", (done) => {
+            emitData(port, "A response.")
+            focus.request("test.command").then((data) => {
+                expect(data).to.be.a("string")
+                expect(data).to.equal("A response.")
+                done()
+            })
+        })
+    })
+
+    describe("#addCommand", () => {
+        it ("can add and call custom commands", (done) => {
+            emitData(port, "add-command")
+
+            const myCommand = (s) => {
+                return s.request("test-command")
+            }
+
+            focus.addCommands({myCommand: myCommand})
+            focus.command("myCommand").then((data) => {
+                expect(data).to.be.a("string")
+                expect(data).to.equal("add-command")
+                done()
+            })
+        })
+    })
+})

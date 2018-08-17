248cdd6274203e0c6c04321ea25af6708beab397
Gergely Nagy <algernon@keyboard.io>
2018-08-17 11:47:25 +0200

    License & copyright update
    Relicensed under the GPLv3-only, and copyright moved to Keyboardio, Inc.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- packages/chrysalis-hardware-keyboardio-model01/src/lib/chrysalis-hardware-keyboardio-model01.js
+++ packages/chrysalis-hardware-keyboardio-model01/src/lib/chrysalis-hardware-keyboardio-model01.js
@@ -1,10 +1,9 @@
 /* chrysalis-hardware-keyboardio-model01 -- Chrysalis Keyboardio Model01 support
- * Copyright (C) 2018  Gergely Nagy
+ * Copyright (C) 2018  Keyboardio, Inc.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of

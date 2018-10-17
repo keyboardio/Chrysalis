dd7a0489df95e4717a04f26a68c0334d8c32d273
Tre Ammatuna <tammatuna@avvo.com>
2018-10-17 12:53:00 -0700

    Add first tests
    Signed-off-by: Tre Ammatuna <tammatuna@avvo.com>

--- src/renderer/App.test.js
+++ src/renderer/App.test.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { render } from "react-testing-library";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  const { container } = render(<App />);
+  expect(container).toMatchSnapshot();
 });

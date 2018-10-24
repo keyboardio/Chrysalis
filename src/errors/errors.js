ca4b21ec76946312952022ee1bf7110850d296d7
Paul Jones <thepauljones@gmail.com>
2018-10-24 17:06:12 +0200

    Return from onKeyChanged if the keyIndex is -1 i.e. you didn't click a key but entered a keycode
    Signed-off-by: Paul Jones <thepauljones@gmail.com>

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -6,5 +6,6 @@ This could be because your firmware is out of date.
 Flash with updated firmware from https://github.com/keyboardio/Model01-Firmware
 
 If you have a customized sketch take a look at
-https://github.com/keyboardio/Model01-Firmware/pull/65/files to see how to manually update.`
+https://github.com/keyboardio/Model01-Firmware/pull/65/files to see how to manually update.`,
+    noKeySelected: `Please click on a key to select one, then enter the keycode for that key and click 'Apply'`
 };

cf172e7991f5ac5a008accee6ed3db3cf62bf8f2
Tre Ammatuna <tre@tretuna.com>
2018-10-25 10:47:15 -0700

    Fix eslint and prettier errors
    Signed-off-by: Tre Ammatuna <tre@tretuna.com>

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -1,11 +1,10 @@
 export default {
-    firmware:
-`Could not communicate with your Model01.
+  firmware: `Could not communicate with your Model01.
 
 This could be because your firmware is out of date.
 Flash with updated firmware from https://github.com/keyboardio/Model01-Firmware
 
 If you have a customized sketch take a look at
 https://github.com/keyboardio/Model01-Firmware/pull/65/files to see how to manually update.`,
-    noKeySelected: `Please click on a key to select one, then enter the keycode for that key and click 'Apply'`
+  noKeySelected: `Please click on a key to select one, then enter the keycode for that key and click 'Apply'`
 };

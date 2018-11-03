cc848603e250f29c1c642c794e18a757979068de
Gergely Nagy <algernon@keyboard.io>
2018-11-03 15:24:07 +0100

    keymap-transformer: Add media controls and misc. keys
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ src/renderer/keys/mediacontrols.js
@@ -0,0 +1,66 @@
+// -*- mode: js-jsx -*-
+/* chrysalis-bundle-keyboardio -- Chrysalis Bundle for Keyboard.io
+ * Copyright (C) 2018  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const MediaControlTable = {
+  groupName: "Media",
+  keys: [
+    {
+      code: 19682,
+      labels: {
+        top: "Media",
+        primary: "Mute"
+      }
+    },
+    {
+      code: 22709,
+      labels: {
+        top: "Media",
+        primary: "Next Trk"
+      }
+    },
+    {
+      code: 22710,
+      labels: {
+        top: "Media",
+        primary: "Prev Trk"
+      }
+    },
+    {
+      code: 22733,
+      labels: {
+        top: "Media",
+        primary: "Play"
+      }
+    },
+    {
+      code: 23785,
+      labels: {
+        top: "Media",
+        primary: "Vol+"
+      }
+    },
+    {
+      code: 23786,
+      labels: {
+        top: "Media",
+        primary: "Volr-+"
+      }
+    }
+  ]
+};
+
+export default MediaControlTable;

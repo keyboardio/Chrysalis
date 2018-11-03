c0cfea85ef1c5c51d489ce3512c69c84568db28a
Gergely Nagy <algernon@keyboard.io>
2018-11-03 11:00:35 +0100

    key-transformer: Instead of trying to be clever, just use a big list
    Since we're not in the firmware where we need to save as much space as possible,
    we can just use a big list with 64k elements for each keycode. Simplified both
    lookup and serialization.
    
    Since not all codes are useful, and we want grouping, this patch sets things up
    accordingly.
    
    Partly addresses #18.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/renderer/keymap-transformer.js
+++ src/renderer/keymap-transformer.js
@@ -15,465 +15,81 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import BlankTable from "./keys/blanks.js";
+import LetterTable from "./keys/letters.js";
+import DigitTable from "./keys/digits.js";
+import LayerSwitchTable from "./keys/layerswitch.js";
+import PunctuationTable from "./keys/punctuation.js";
+import SpacingTable from "./keys/spacing.js";
+import ModifiersTable from "./keys/modifiers.js";
+import NavigationTable from "./keys/navigation.js";
+import LEDEffectsTable from "./keys/ledeffects.js";
+import MacrosTable from "./keys/macros.js";
+import NumpadTable from "./keys/numpad.js";
+import FunctionKeyTable from "./keys/fxs.js";
+
+//import MediaControlTable from "./keys/mediacontrol.js"
+//import MouseControlTable from "./keys/mousecontrol.js"
+
+const keyCodeTable = [
+  LetterTable,
+  DigitTable,
+  PunctuationTable,
+  SpacingTable,
+  ModifiersTable,
+  NavigationTable,
+  FunctionKeyTable,
+  NumpadTable,
+
+  LayerSwitchTable,
+
+  LEDEffectsTable,
+  MacrosTable,
+  // MediaControlTable,
+  // MouseControlTable,
+
+  BlankTable
+];
+
 class DisplayTransformer {
   constructor() {
-    this.hid_code_map = [
-      "",
-      undefined,
-      undefined,
-      undefined,
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "0",
-      "Enter",
-      "Esc",
-      "BSpc",
-      "Tab",
-      "Space",
-      "-",
-      "=",
-      "[",
-      "]",
-      "\\",
-      "NonUsPound",
-      ";",
-      "'",
-      "`",
-      ",",
-      ".",
-      "/",
-      "CapsLK",
-      "F1",
-      "F2",
-      "F3",
-      "F4",
-      "F5",
-      "F6",
-      "F7",
-      "F8",
-      "F9",
-      "F10",
-      "F11",
-      "F12",
-      "PrnScr",
-      "ScrlLK",
-      "Pause",
-      "Insert",
-      "Home",
-      "PgUp",
-      "Del",
-      "End",
-      "PgDn",
-      "→",
-      "←",
-      "↓",
-      "↑",
-      "KeypadNumLock",
-      "KeypadDivide",
-      "KeypadMultiply",
-      "KeypadSubtract",
-      "KeypadAdd",
-      "KeypadEnter",
-      "Keypad1",
-      "Keypad2",
-      "Keypad3",
-      "Keypad4",
-      "Keypad5",
-      "Keypad6",
-      "Keypad7",
-      "Keypad8",
-      "Keypad9",
-      "Keypad0",
-      "KeypadDot",
-      "KeypadNonUsBackslashAndPipe",
-      "App",
-      "Power",
-      "KeypadEquals",
-      "F13",
-      "F14",
-      "F15",
-      "F16",
-      "F17",
-      "F18",
-      "F19",
-      "F20",
-      "F21",
-      "F22",
-      "F23",
-      "F24",
-      "Execute",
-      "Help",
-      "Menu",
-      "Select",
-      "Stop",
-      "Again",
-      "Undo",
-      "Cut",
-      "Copy",
-      "Paste",
-      "Find",
-      "Mute",
-      "VolumeUp",
-      "VolumeDown",
-      "LockingCapsLock",
-      "LockingNumLock",
-      "LockingScrollLock",
-      "KeypadComma",
-      "KeypadEqualSign",
-      "International1",
-      "International2",
-      "International3",
-      "International4",
-      "International5",
-      "International6",
-      "International7",
-      "International8",
-      "International9",
-      "Lang1",
-      "Lang2",
-      "Lang3",
-      "Lang4",
-      "Lang5",
-      "Lang6",
-      "Lang7",
-      "Lang8",
-      "Lang9",
-      "AlternateErase",
-      "Sysreq",
-      "Cancel",
-      "Clear",
-      "Prior",
-      "Return",
-      "Separator",
-      "Out",
-      "Oper",
-      "ClearSlashAgain",
-      "CrselSlashProps",
-      "Exsel",
-      undefined, // 0xA5
-      undefined, // 0xA6
-      undefined, // 0xA7
-      undefined, // 0xA8
-      undefined, // 0xA9
-      undefined, // 0xAA
-      undefined, // 0xAB
-      undefined, // 0xAC
-      undefined, // 0xAD
-      undefined, // 0xAE
-      undefined, // 0xAF
-      "Keypad00",
-      "Keypad000",
-      "KeypadThousandsSeparator",
-      "DecimalSeparator",
-      "CurrencyUnit",
-      "CurrencySubunit",
-      "KeypadLeftParen",
-      "KeypadRightParen",
-      "KeypadLeftCurlyBrace",
-      "KeypadRightCurlyBrace",
-      "KeypadTab",
-      "KeypadBackspace",
-      "KeypadA",
-      "KeypadB",
-      "KeypadC",
-      "KeypadD",
-      "KeypadE",
-      "KeypadF",
-      "KeypadXor",
-      "KeypadCarat",
-      "KeypadPercent",
-      "KeypadLessThan",
-      "KeypadGreaterThan",
-      "KeypadAmpersand",
-      "KeypadDoubleampersand",
-      "KeypadPipe",
-      "KeypadDoublepipe",
-      "KeypadColon",
-      "KeypadPoundSign",
-      "KeypadSpace",
-      "KeypadAtSign",
-      "KeypadExclamationPoint",
-      "KeypadMemoryStore",
-      "KeypadMemoryRecall",
-      "KeypadMemoryClear",
-      "KeypadMemoryAdd",
-      "KeypadMemorySubtract",
-      "KeypadMemoryMultiply",
-      "KeypadMemoryDivide",
-      "KeypadPlusSlashMinus",
-      "KeypadClear",
-      "KeypadClearEntry",
-      "KeypadBinary",
-      "KeypadOctal",
-      "KeypadDecimal",
-      "KeypadHexadecimal",
-      undefined, // 0xDE
-      undefined, // 0xDF
-      "LCtrl",
-      "LShift",
-      "LAlt",
-      "LGui",
-      "RCtrl",
-      "RShift",
-      "RAlt",
-      "RGui"
-    ];
+    this.keymapCodeTable = [];
+
+    for (let group of keyCodeTable) {
+      for (let key of group.keys) {
+        let value;
+
+        if (key.labels.keymap) {
+          value = key.labels.keymap;
+        } else if (key.labels.all) {
+          value = key.labels.all;
+        } else {
+          value = "#" + key.code.toString();
+        }
+
+        this.keymapCodeTable[key.code] = value;
+      }
+    }
   }
 
   parse(keyCode) {
-    let label = keyCode.toString(),
-      extraLabel = null;
+    let key;
 
-    if (keyCode <= this.hid_code_map.length) {
-      label = this.hid_code_map[keyCode];
-      if (label && label.startsWith("Keypad")) {
-        extraLabel = "Keypad";
-        label = label.substr(6);
-      }
-    } else if (keyCode == 65535) {
-      label = "[Trns]";
-    } else if (keyCode & 0b1000000000000000) {
-      // Reserved keys
+    if (keyCode < this.keymapCodeTable.length) {
+      key = this.keymapCodeTable[keyCode];
+    }
 
-      label = "SPEC";
-
-      if (keyCode >= 0xc001 && keyCode <= 0xc008) {
-        // OSM
-        let modifier = keyCode - 0xc000 + this.hid_code_map.indexOf("LCtrl");
-        label = this.hid_code_map[modifier];
-        extraLabel = "OneShot";
-      }
-      if (keyCode >= 0xc009 && keyCode <= 0xc010) {
-        // OSL
-        label = "L#" + (keyCode - 0xc009).toString();
-        extraLabel = "OneShot";
-      }
-      if (keyCode >= 0xc011 && keyCode <= 0xc811) {
-        // TODO: DUM
-        extraLabel = "DualUse";
-        label = "??";
-      }
-      if (keyCode >= 0xc812 && keyCode <= 0xd012) {
-        // TODO: DUL
-        extraLabel = "DualUse";
-        label = "??";
-      }
-      if (keyCode >= 0xd013 && keyCode <= 0xd022) {
-        // TD
-        extraLabel = "TD";
-        label = "(" + (keyCode - 0xd013).toString() + ")";
-      }
-      if (keyCode >= 0xd023 && keyCode <= 0xd02a) {
-        // LEAD
-        extraLabel = "Leader";
-        label = "(" + (keyCode - 0xd023).toString() + ")";
-      }
-      if (keyCode == 0xd02b) {
-        // CYCLE
-        label = "CYCLE";
-      }
-      if (keyCode == 0xd02c) {
-        // Syster
-        label = "SYSTER";
-      }
-      if (keyCode >= 0xd02d && keyCode <= 0xd12c) {
-        // Topsy
-        extraLabel = "Topsy";
-        let topsyKeyCode = keyCode - 0xd02d;
-        label = this.hid_code_map[topsyKeyCode];
-      }
-      if (keyCode >= 0xd12d && keyCode <= 0xd157) {
-        // TODO: Steno
-        extraLabel = "Steno";
-        label = "??";
-      }
-      if (keyCode >= 0xd158 && keyCode <= 0xd159) {
-        // TODO: SpaceCadet
-        extraLabel = "SC";
-        label = "??";
-      }
-    } else {
-      // Core keys
-      let key = keyCode % 256,
-        flags = (keyCode - key) / 256;
-
-      extraLabel = flags.toString();
-      label = key.toString();
-
-      if (flags & 0b01000000) {
-        // Synthetic
-
-        if (flags & 0b00100000) {
-          // Macros
-          extraLabel = "Macro";
-          label = "(" + key.toString() + ")";
-        } else if (flags & 0b00001000) {
-          // Consumer
-          extraLabel = "Consumer";
-          let consumerKey = keyCode & 0x03ff;
-          label = consumerKey.toString();
-
-          // TODO: Implement the full consumer lookup table a'la the HID map.
-          if (consumerKey == 181) label = "N. Track";
-          if (consumerKey == 182) label = "P. Track";
-          if (consumerKey == 205) label = "Play";
-          if (consumerKey == 226) label = "Mute";
-          if (consumerKey == 233) label = "Vol-";
-          if (consumerKey == 234) label = "Vol+";
-        } else if (flags & 0b00000100) {
-          // Layer keys
-          let layer = key;
-          if (layer >= 42) {
-            extraLabel = "ShiftTo";
-            label = (layer - 42).toString();
-          } else {
-            label = layer.toString();
-            extraLabel = "LockTo";
-          }
-        } else if (flags & 0b00000011) {
-          // LED Next / prev
-          if (key == 0) {
-            label = "Next";
-          } else {
-            label = "Prev";
-          }
-          extraLabel = "LEDEff.";
-        } else if (flags & 0b00010000) {
-          // MouseKeys
-          extraLabel = "Mouse";
-
-          if (key & 0b0100000) {
-            // warp
-            extraLabel = "M. Warp";
-            label = "";
-            if (key & 0b0000001) {
-              label += "N";
-            }
-            if (key & 0b0000010) {
-              label += "S";
-            }
-            if (key & 0b0000100) {
-              label += "W";
-            }
-            if (key & 0b0001000) {
-              label += "E";
-            }
-
-            if (key & 0b1000000) {
-              label = "WarpEnd";
-            }
-          } else if (key & 0b0010000) {
-            // wheel
-            extraLabel = "M. wheel";
-            label = "";
-
-            if (key & 0b0000001) {
-              label += "Up";
-            }
-            if (key & 0b0000010) {
-              label += "Down";
-            }
-            if (key & 0b0000100) {
-              label += "Left";
-            }
-            if (key & 0b0001000) {
-              label += "Right";
-            }
-          } else if (key & 0b1000000) {
-            // buttons
-            extraLabel = "M. Btn";
-            let button = key;
-            if (button & (1 << 0)) label = "Left";
-            if (button & (1 << 1)) label = "Right";
-            if (button & (1 << 2)) label = "Middle";
-            if (button & (1 << 3)) label = "Next";
-            if (button & (1 << 4)) label = "Prev";
-          } else {
-            // Movement
-            label = "";
-            if (key & 0b0000001) {
-              label += "Up";
-            }
-            if (key & 0b0000010) {
-              label += "Down";
-            }
-            if (key & 0b0000100) {
-              label += "Left";
-            }
-            if (key & 0b0001000) {
-              label += "Right";
-            }
-          }
-        }
-      } else {
-        // Held keys
-        extraLabel = "";
-
-        let hold = mod => {
-          if (extraLabel) {
-            extraLabel += "+" + mod;
-          } else {
-            extraLabel = mod;
-          }
-        };
-
-        if (flags & 0b00000001) {
-          hold("Ctrl");
-        }
-        if (flags & 0b00000010) {
-          hold("Alt");
-        }
-        if (flags & 0b00000100) {
-          hold("AltGr");
-        }
-        if (flags & 0b00001000) {
-          hold("Shift");
-        }
-        if (flags & 0b00010000) {
-          hold("GUI");
-        }
-
-        label = this.hid_code_map[key];
-      }
+    if (!key) {
+      key = {
+        code: keyCode,
+        primary: "#" + keyCode.toString()
+      };
     }
 
     return {
-      keyCode: keyCode,
-      label: label,
-      extraLabel: extraLabel
+      keyCode: key.code,
+      label: key.primary,
+      extraLabel: key.top
     };
   }
 
---
+++ src/renderer/keys/macros.js
@@ -0,0 +1,43 @@
+// -*- mode: js-jsx -*-
+/* chrysalis-bundle-keyboardio -- Chrysalis Bundle for Keyboard.io
+ * Copyright (C) 2018  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const MacrosTable = {
+  groupName: "Macros",
+  keys: [
+    {
+      code: 24576,
+      labels: {
+        keymap: {
+          primary: "#0",
+          top: "Macro"
+        }
+      }
+    },
+    {
+      code: 24577,
+      labels: {
+        keymap: {
+          primary: "#1",
+          top: "Macro"
+        }
+      }
+    }
+    // TODO: There are 256 macro keys
+  ]
+};
+
+export default MacrosTable;

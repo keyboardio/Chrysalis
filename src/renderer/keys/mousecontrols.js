67ebea9f2a6f3042b65eb4adbc5643728eb287a9
Gergely Nagy <algernon@keyboard.io>
2018-11-03 15:16:29 +0100

    key-transformer: mouse controls
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ src/renderer/keys/mousecontrols.js
@@ -0,0 +1,108 @@
+// -*- mode: js-jsx -*-
+/* chrysalis-bundle-keyboardio -- Chrysalis Bundle for Keyboard.io
+ * Copyright (C) 2018  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const MouseControlTable = {
+  groupName: "Mouse",
+  keys: [
+    {
+      code: 20481,
+      labels: {
+        top: "Mouse",
+        primary: "Up"
+      }
+    },
+    {
+      code: 20482,
+      labels: {
+        top: "Mouse",
+        primary: "Down"
+      }
+    },
+    {
+      code: 20484,
+      labels: {
+        top: "Mouse",
+        primary: "Left"
+      }
+    },
+    {
+      code: 20488,
+      labels: {
+        top: "Mouse",
+        primary: "Right"
+      }
+    },
+    {
+      code: 20545,
+      labels: {
+        top: "M. Btn",
+        primary: "Left"
+      }
+    },
+    {
+      code: 20546,
+      labels: {
+        top: "M. Btn",
+        primary: "Right"
+      }
+    },
+    {
+      code: 20548,
+      labels: {
+        top: "M. Btn",
+        primary: "Middle"
+      }
+    },
+    {
+      code: 20576,
+      labels: {
+        top: "M. Warp",
+        primary: "End"
+      }
+    },
+    {
+      code: 20517,
+      labels: {
+        top: "M. Warp",
+        primary: "NW"
+      }
+    },
+    {
+      code: 20518,
+      labels: {
+        top: "M. Warp",
+        primary: "SW"
+      }
+    },
+    {
+      code: 20521,
+      labels: {
+        top: "M. Warp",
+        primary: "NE"
+      }
+    },
+    {
+      code: 20522,
+      labels: {
+        top: "M. Warp",
+        primary: "SE"
+      }
+    }
+  ]
+};
+
+export default MouseControlTable;

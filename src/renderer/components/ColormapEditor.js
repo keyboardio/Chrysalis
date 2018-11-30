f4abf1eac194598839c5900029ce417e74c0957d
Gergely Nagy <algernon@keyboard.io>
2018-11-30 23:35:39 +0100

    Add a colormap editor
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ src/renderer/components/ColormapEditor.js
@@ -0,0 +1,203 @@
+// -*- mode: js-jsx -*-
+/* chrysalis-bundle-keyboardio -- Chrysalis Bundle for Keyboard.io
+ * Copyright (C) 2018  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import Toolbar from "@material-ui/core/Toolbar";
+import { withStyles } from "@material-ui/core/styles";
+
+import Focus from "chrysalis-focus";
+
+import Palette from "./ColormapEditor/Palette";
+import Layer, { led_map } from "./ColormapEditor/Layer";
+
+const styles = theme => ({
+  tabs: {
+    flexGrow: 1
+  },
+  editor: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3
+  },
+  palette: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
+
+class ColormapEditor extends React.Component {
+  state = {
+    currentLayer: 0,
+    currentKeyIndex: -1,
+    modified: false,
+    saving: false,
+    selectedPaletteColor: -1,
+    palette: [],
+    colorMap: []
+  };
+
+  scanKeyboard = async () => {
+    let focus = new Focus();
+
+    console.log("Pulling the colormap...");
+    let colormap = await focus.command("colormap");
+
+    this.setState({
+      palette: colormap.palette,
+      colorMap: colormap.colorMap
+    });
+
+    console.log("done!");
+  };
+
+  selectLayer = (event, value) => {
+    this.setState({
+      currentLayer: value,
+      currentKeyIndex: -1
+    });
+  };
+
+  onColorSelect = colorIndex => {
+    this.setState({ selectedPaletteColor: colorIndex });
+  };
+
+  onColorPick = (colorIndex, r, g, b) => {
+    let newPalette = this.state.palette.slice();
+    newPalette[colorIndex] = {
+      r: r,
+      g: g,
+      b: b,
+      rgb: `rgb(${r}, ${g}, ${b})`
+    };
+    this.setState({
+      palette: newPalette,
+      modified: true
+    });
+  };
+
+  onKeySelect = event => {
+    event.preventDefault();
+    let layer = parseInt(event.currentTarget.getAttribute("data-layer")),
+      row = parseInt(event.currentTarget.getAttribute("data-key-row")),
+      col = parseInt(event.currentTarget.getAttribute("data-key-col")),
+      keyIndex = row * 16 + col,
+      ledIndex = led_map[row][col];
+    this.setState({
+      currentLayer: layer,
+      currentKeyIndex: keyIndex
+    });
+    if (this.state.selectedPaletteColor != -1) {
+      this.setState(state => {
+        let colormap = state.colorMap.slice();
+        colormap[layer][ledIndex] = this.state.selectedPaletteColor;
+        return colormap;
+      });
+      this.setState({ modified: true });
+    }
+  };
+
+  onApply = async event => {
+    event.preventDefault();
+    this.setState({ saving: true });
+    let focus = new Focus();
+    await focus.command("colormap", this.state.palette, this.state.colorMap);
+    this.setState({
+      modified: false,
+      saving: false
+    });
+    console.log("colormap updated");
+  };
+
+  render() {
+    if (this.state.colorMap.length == 0) {
+      this.scanKeyboard();
+      return (
+        <main>
+          <CircularProgress />
+        </main>
+      );
+    }
+
+    const { classes } = this.props;
+    let saveButtonContent = "Save changes";
+    if (this.state.saving) {
+      saveButtonContent = <CircularProgress color="inherit" size={16} />;
+    }
+
+    let tabs = this.state.colorMap.map((_, index) => {
+      let label = "Layer #" + index.toString(),
+        tabKey = "tab-layer-" + index.toString();
+      return <Tab label={label} key={tabKey} />;
+    });
+
+    let colormap = (
+      <Layer
+        index={this.state.currentLayer}
+        palette={this.state.palette}
+        colormap={this.state.colorMap[this.state.currentLayer]}
+        onKeySelect={this.onKeySelect}
+        selectedKey={this.state.currentKeyIndex}
+      />
+    );
+
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Tabs
+              className={classes.tabs}
+              value={this.state.currentLayer}
+              scrollable
+              scrollButtons="auto"
+              onChange={this.selectLayer}
+            >
+              {tabs}
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.editor}>
+          {colormap}
+          <Palette
+            palette={this.state.palette}
+            className={classes.palette}
+            onColorSelect={this.onColorSelect}
+            onColorPick={this.onColorPick}
+          />
+        </div>
+        <Button
+          onClick={this.onApply}
+          variant="contained"
+          color="secondary"
+          disabled={!this.state.modified}
+        >
+          {saveButtonContent}
+        </Button>
+      </div>
+    );
+  }
+}
+
+ColormapEditor.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ColormapEditor);
---
+++ src/renderer/components/ColormapEditor/Key.js
@@ -0,0 +1,67 @@
+// -*- mode: js-jsx -*-
+/* chrysalis-bundle-keyboardio -- Chrysalis Bundle for Keyboard.io
+ * Copyright (C) 2018  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+
+class Key extends React.Component {
+  render() {
+    let shape,
+      stroke = "#b3b3b3";
+
+    if (this.props.active) {
+      stroke = "#f3b3b3";
+    }
+
+    if (this.props.palmKey) {
+      shape = (
+        <ellipse
+          fill={this.props.color}
+          stroke={stroke}
+          strokeWidth="2.73"
+          cx="610.765"
+          cy="953.469"
+          rx="75.6"
+          ry="56.001"
+          transform={this.props.shape}
+        />
+      );
+    } else {
+      shape = (
+        <path
+          fill={this.props.color}
+          stroke={stroke}
+          strokeWidth="1.55"
+          d={this.props.shape}
+        />
+      );
+    }
+
+    return (
+      <g
+        onClick={this.props.onClick}
+        className="key"
+        data-key-row={this.props.row}
+        data-key-col={this.props.col}
+        data-layer={this.props.layer}
+      >
+        {shape}
+      </g>
+    );
+  }
+}
+
+export default Key;

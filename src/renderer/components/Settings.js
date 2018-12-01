bab896b89a6b12990613e3746decaa41017b5993
Gergely Nagy <algernon@keyboard.io>
2018-12-01 11:00:53 +0100

    Settings: Add a margin
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/renderer/components/Settings.js
+++ src/renderer/components/Settings.js
@@ -26,7 +26,8 @@ const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2
+    paddingBottom: theme.spacing.unit * 2,
+    margin: theme.spacing.unit * 3
   }
 });
 

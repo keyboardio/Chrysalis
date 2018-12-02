ca76451f03203a494037ae6e9524065e4111855b
Gergely Nagy <algernon@keyboard.io>
2018-12-02 10:12:51 +0100

    eslint: Set env.es6 to true
    We want to use Promise.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,11 @@ module.exports = {
   ],
   parser: "babel-eslint",
   parserOptions: {
+    ecmaVersion: 2017,
     sourceType: "module"
   },
   env: {
+    es6: true,
     browser: true,
     node: true
   },

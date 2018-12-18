0d4bce8f8bfa3b2ed536f47f49f4b5a6d9d6aa38
Gergely Nagy <algernon@keyboard.io>
2018-12-19 00:50:43 +0100

    chrysalis-colormap: Make it eslint-clean
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- packages/chrysalis-colormap/src/lib/chrysalis-colormap.js
+++ packages/chrysalis-colormap/src/lib/chrysalis-colormap.js
@@ -15,85 +15,85 @@
  */
 
 export default class Colormap {
-  constructor(opts) {
-    this.setLayerSize(opts);
-  }
-
-  setLayerSize(opts) {
-    if (!opts || opts == undefined) return;
-
-    if (typeof opts == "number") {
-      this._layerSize = opts;
-    } else if (typeof opts == "object") {
-      this._layerSize = opts.keyboard.rows * opts.keyboard.columns;
+    constructor(opts) {
+        this.setLayerSize(opts)
     }
 
-    return this;
-  }
+    setLayerSize(opts) {
+        if (!opts || opts == undefined) return
 
-  _chunk(a, chunkSize) {
-    var R = [];
-    for (var i = 0; i < a.length; i += chunkSize)
-      R.push(a.slice(i, i + chunkSize));
-    return R;
-  }
+        if (typeof opts == "number") {
+            this._layerSize = opts
+        } else if (typeof opts == "object") {
+            this._layerSize = opts.keyboard.rows * opts.keyboard.columns
+        }
 
-  async _pull(s) {
-    let paletteData = await s.request("palette");
-    let colorMapData = await s.request("colormap.map");
-
-    let palette = this._chunk(
-      paletteData
-        .split(" ")
-        .filter(v => v.length > 0)
-        .map(k => parseInt(k)),
-      3
-    ).map(color => {
-      return {
-        r: color[0],
-        g: color[1],
-        b: color[2],
-        rgb: `rgb(${color[0]}, ${color[1]}, ${color[2]})`
-      };
-    });
-
-    let colorMap = this._chunk(
-      colorMapData
-        .split(" ")
-        .filter(v => v.length > 0)
-        .map(k => parseInt(k)),
-      this._layerSize
-    );
-
-    return {
-      palette: palette,
-      colorMap: colorMap
-    };
-  }
-
-  _flatten (arr) {
-    return [].concat(...arr);
-  };
-
-  async _updatePalette(s, palette) {
-    let args = this._flatten(
-      palette.map(color => [color.r, color.g, color.b])
-    ).map(v => v.toString());
-
-    return await s.request("palette", ...args);
-  }
-
-  async _updateColormap(s, colormap) {
-    let args = this._flatten(colormap).map(v => v.toString());
-    return await s.request("colormap.map", ...args);
-  }
-
-  async focus(s, palette, colormap) {
-    if (!palette && !colormap) {
-      return this._pull(s);
+        return this
     }
 
-    if (palette) await this._updatePalette(s, palette);
-    if (colormap) await this._updateColormap(s, colormap);
-  }
+    _chunk(a, chunkSize) {
+        var R = []
+        for (var i = 0; i < a.length; i += chunkSize)
+            R.push(a.slice(i, i + chunkSize))
+        return R
+    }
+
+    async _pull(s) {
+        let paletteData = await s.request("palette")
+        let colorMapData = await s.request("colormap.map")
+
+        let palette = this._chunk(
+            paletteData
+                .split(" ")
+                .filter(v => v.length > 0)
+                .map(k => parseInt(k)),
+            3
+        ).map(color => {
+            return {
+                r: color[0],
+                g: color[1],
+                b: color[2],
+                rgb: `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+            }
+        })
+
+        let colorMap = this._chunk(
+            colorMapData
+                .split(" ")
+                .filter(v => v.length > 0)
+                .map(k => parseInt(k)),
+            this._layerSize
+        )
+
+        return {
+            palette: palette,
+            colorMap: colorMap
+        }
+    }
+
+    _flatten (arr) {
+        return [].concat(...arr)
+    }
+
+    async _updatePalette(s, palette) {
+        let args = this._flatten(
+            palette.map(color => [color.r, color.g, color.b])
+        ).map(v => v.toString())
+
+        return await s.request("palette", ...args)
+    }
+
+    async _updateColormap(s, colormap) {
+        let args = this._flatten(colormap).map(v => v.toString())
+        return await s.request("colormap.map", ...args)
+    }
+
+    async focus(s, palette, colormap) {
+        if (!palette && !colormap) {
+            return this._pull(s)
+        }
+
+        if (palette) await this._updatePalette(s, palette)
+        if (colormap) await this._updateColormap(s, colormap)
+    }
 }

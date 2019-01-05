0780ddb14624e0c68215f50316bf424434827760
Gergely Nagy <algernon@keyboard.io>
2019-01-05 15:07:23 +0100

    ColormapEditor: Major redesign
    Similar to #138, this moves the palette selector to the bottom. Also switched to
    `MaterialPicker`, and improved the palette "boxes" (now circles).
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/renderer/screens/ColormapEditor/Palette.js
+++ src/renderer/screens/ColormapEditor/Palette.js
@@ -17,15 +17,29 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 
+import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
+
+import { MaterialPicker } from "react-color";
 
 import ColorBox from "./ColorBox";
 
-const styles = () => ({
-  palette: {
-    display: "flex"
+const styles = theme => ({
+  root: {
+    display: "flex",
+    position: "fixed",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 150,
+    padding: theme.spacing.unit * 2,
+    justifyContent: "center"
+  },
+  picker: {
+    width: 124,
+    display: "flex",
+    justifyContent: "center"
   }
 });
 
@@ -40,15 +54,18 @@ class Palette extends React.Component {
     this.props.onColorSelect(colorIndex);
   };
 
+  onColorPick = ({ rgb: { r, g, b } }) => {
+    this.props.onColorPick(this.state.selectedColor, r, g, b);
+  };
+
   render() {
     if (!this.props.palette) return null;
 
     const { classes, palette } = this.props;
 
-    let [lowHalf, highHalf] = [palette.slice(0, 8), palette.slice(8)];
-
-    let lowWidget = lowHalf.map((color, index) => {
-      let itemKey = "palette-index-" + index.toString();
+    const [lowHalf, highHalf] = [palette.slice(0, 8), palette.slice(8)];
+    const lowWidget = lowHalf.map((color, index) => {
+      const itemKey = "palette-index-" + index.toString();
       return (
         <ColorBox
           selected={index == this.state.selectedColor}
@@ -56,12 +73,11 @@ class Palette extends React.Component {
           color={color}
           colorIndex={index}
           onColorSelect={this.onColorSelect}
-          onColorPick={this.props.onColorPick}
         />
       );
     });
-    let highWidget = highHalf.map((color, index) => {
-      let itemKey = "palette-index-" + (index + 8).toString();
+    const highWidget = highHalf.map((color, index) => {
+      const itemKey = "palette-index-" + (index + 8).toString();
       return (
         <ColorBox
           selected={index + 8 == this.state.selectedColor}
@@ -69,20 +85,21 @@ class Palette extends React.Component {
           color={color}
           colorIndex={index + 8}
           onColorSelect={this.onColorSelect}
-          onColorPick={this.props.onColorPick}
         />
       );
     });
 
+    const color = this.props.palette[this.state.selectedColor];
+    const picker = this.state.selectedColor != -1 && (
+      <MaterialPicker color={color} onChangeComplete={this.onColorPick} />
+    );
+
     return (
-      <div className={classes.palette}>
-        <div className={classNames(classes.palette, this.props.className)}>
-          {lowWidget}
-        </div>
-        <div className={classNames(classes.palette, this.props.className)}>
-          {highWidget}
-        </div>
-      </div>
+      <Paper className={classes.root} square>
+        {lowWidget}
+        <div className={classes.picker}>{picker}</div>
+        {highWidget}
+      </Paper>
     );
   }
 }

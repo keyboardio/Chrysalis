e0ff42cb2aafc7b07a92bfd39d1bf36c4d38ac64
Gergely Nagy <algernon@keyboard.io>
2019-01-19 08:19:01 +0100

    chrysalis-flash: Split up Avr109, and allow configurable timeouts
    Split up Avr109 into two parts: one that does the reset, and another that does
    the flashing. This is so that if we have a device already in bootloader mode,
    we'd still be able to flash it, without having to reset it again.
    
    While there, introduce configurable timeouts with nice names, and bump the time
    we allow the bootloader to come up to 2s. The names fix #15.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- packages/chrysalis-flash/src/lib/chrysalis-flash.js
+++ packages/chrysalis-flash/src/lib/chrysalis-flash.js
@@ -17,7 +17,41 @@
 import AvrGirl from "avrgirl-arduino"
 import TeensyLoader from "teensy-loader"
 
-async function Avr109(board, port, filename) {
+async function Avr109Bootloader(board, port, filename, timeouts) {
+  timeouts = timeouts || {
+    close: 500 // Time to wait (ms) between the end of flash and closing the
+               // device
+  }
+  const avrgirl = new AvrGirl({
+    board: board,
+    debug: true,
+    manualReset: true
+  })
+
+  return new Promise((resolve, reject) => {
+    avrgirl.flash(filename, error => {
+      if (error) {
+        console.log(error)
+        try {
+          avrgirl.connection.serialPort.close()
+        } catch (_) { /* ignore the error */ }
+        reject(error)
+      } else {
+        setTimeout(() => {
+          avrgirl.connection.serialPort.close()
+          resolve()
+        }, timeouts.close)
+      }
+    })
+  })
+}
+
+async function Avr109(board, port, filename, timeouts) {
+  timeouts = timeouts || {
+    dtrToggle: 250,    // Time to wait (ms) between toggling DTR
+    bootLoaderUp: 2000 // Time to wait for the boot loader to come up
+  }
+
   return new Promise((resolve, reject) => {
     port.update({ baudRate: 1200 }, () => {
       console.log("baud update")
@@ -28,31 +62,17 @@ async function Avr109(board, port, filename) {
             port.set({ dtr: false }, () => {
               console.log("dtr off")
               setTimeout(() => {
-                let avrgirl = new AvrGirl({
-                  board: board,
-                  debug: true,
-                  manualReset: true
-                })
-
-                avrgirl.flash(filename, error => {
-                  if (error) {
-                    console.log(error)
-                    try {
-                      avrgirl.connection.serialPort.close()
-                    } catch (_) { /* ignore the error */ }
-                    reject(error)
-                  } else {
-                    setTimeout(() => {
-                      avrgirl.connection.serialPort.close()
-                      resolve()
-                    }, 500)
-                  }
-                })
-              }, 1000)
+                try {
+                  Avr109Bootloader(board, port, filename, timeouts)
+                  resolve()
+                } catch (e) {
+                  reject(e)
+                }
+              }, timeouts.bootLoaderUp)
             })
-          }, 250)
+          }, timeouts.dtrToggle)
         })
-      }, 250)
+      }, timeouts.dtrToggle)
     })
   })
 }
@@ -61,4 +81,4 @@ async function teensy(filename) {
   return TeensyLoader.upload(0x16C0, 0x0478, filename)
 }
 
-export { Avr109, teensy }
+export { Avr109, Avr109Bootloader, teensy }

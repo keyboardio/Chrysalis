4c000b2df2255f88891a97ac7cc5ef2c8c627d4c
Gergely Nagy <algernon@keyboard.io>
2019-01-19 11:13:17 +0100

    chrysalis-hardware: New library to collect all the other hardware libs
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ packages/chrysalis-hardware/src/lib/index.js
@@ -0,0 +1,28 @@
+/* chrysalis-hardware -- Chrysalis Hardware library collection
+ * Copyright (C) 2019  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Model01, Model01Bootloader } from "@chrysalis-api/hardware-keyboardio-model01"
+import { Atreus } from "@chrysalis-api/hardware-technomancy-atreus"
+import { Raise } from "@chrysalis-api/hardware-dygma-raise"
+import { ErgoDox } from "@chrysalis-api/hardware-ez-ergodox"
+import { GenericTeensy } from "@chrysalis-api/hardware-pjrc-teensy"
+
+const Hardware = {
+  serial: [Model01, Raise, ErgoDox, Atreus],
+  nonSerial: [Model01Bootloader, ErgoDox, Atreus, GenericTeensy]
+}
+
+export { Hardware as default }

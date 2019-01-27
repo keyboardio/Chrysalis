e8c05386371d42b8e198693145096c223d8e675d
Tré Ammatuna <tretuna@gmail.com>
2019-01-27 15:43:04 -0800

    Update eslint settings
    Signed-off-by: Tré Ammatuna <tretuna@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,23 @@ module.exports = {
   rules: {
     "no-console": 0,
     "react/prop-types": 0
+  },
+  settings: {
+    react: {
+      pragma: "React",  // Pragma to use, default to "React"
+      version: "detect", // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+    },
+    propWrapperFunctions: [
+      // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+      "forbidExtraProps",
+      { "property": "freeze", "object": "Object" },
+      { "property": "myFavoriteWrapper" }
+    ],
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      "Hyperlink",
+      { "name": "Link", "linkAttribute": "to" }
+    ]
   }
 };

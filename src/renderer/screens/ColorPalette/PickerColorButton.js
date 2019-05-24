748f3cc6ff21a36fb9e14e5fd7e32b6f2adf169f
Roman Danilov <r.danylov@gmail.com>
2019-05-24 20:00:31 +0300

    pdates: reset yarn.lock, package.json to the master version, copyright headers added (#381)
    Signed-off-by: Roman Danilov <r.danylov@gmail.com>

--- src/renderer/screens/ColorPalette/PickerColorButton.js
+++ src/renderer/screens/ColorPalette/PickerColorButton.js
@@ -1,3 +1,20 @@
+// -*- mode: js-jsx -*-
+/* Chrysalis -- Kaleidoscope Command Center
+ * Copyright (C) 2018, 2019  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * This is Reactjs functional component that create button to choose colors from Color Picker
  */

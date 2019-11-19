af0f84f830fcb2fa07bde73879fbf5bcc5ab9293
Gergely Nagy <algernon@keyboard.io>
2019-11-19 09:43:09 +0100

    hardware: Disable the Raise bootloaders for now
    Previously, we removed the Raise bootloader components from the lists provided
    by the package, but we did not remove the imports, making the linter unhappy.
    
    I still need to figure out why including this two cause issues, but lets make
    the linter happy meanwhile.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- packages/chrysalis-hardware/src/lib/index.js
+++ packages/chrysalis-hardware/src/lib/index.js
@@ -17,8 +17,8 @@
 import { Model01, Model01Bootloader } from "@chrysalis-api/hardware-keyboardio-model01"
 import { Atreus } from "@chrysalis-api/hardware-technomancy-atreus"
 import { Atreus2 } from "@chrysalis-api/hardware-keyboardio-atreus2"
-import { Raise_ANSI, Raise_ANSIBootloader } from "@chrysalis-api/hardware-dygma-raise-ansi";
-import { Raise_ISO, Raise_ISOBootloader } from "@chrysalis-api/hardware-dygma-raise-iso";
+import { Raise_ANSI } from "@chrysalis-api/hardware-dygma-raise-ansi";
+import { Raise_ISO } from "@chrysalis-api/hardware-dygma-raise-iso";
 import { ErgoDox } from "@chrysalis-api/hardware-ez-ergodox"
 import { Planck } from "@chrysalis-api/hardware-olkb-planck"
 import { KBD4x } from "@chrysalis-api/hardware-kbdfans-kbd4x"

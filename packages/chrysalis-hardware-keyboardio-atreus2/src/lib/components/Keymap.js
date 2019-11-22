b7bd6ef39a09fe043f23d869a938638db91fb332
Gergely Nagy <algernon@keyboard.io>
2019-11-22 15:41:51 +0100

    hardware/atreus2: Rotate the left and right halves
    Rotate the left and right halves 10 degrees, to match the real hardware.
    
    Fixes keyboardio/Chrysalis#443.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- packages/chrysalis-hardware-keyboardio-atreus2/src/lib/components/Keymap.js
+++ packages/chrysalis-hardware-keyboardio-atreus2/src/lib/components/Keymap.js
@@ -38,9 +38,8 @@ class Keymap extends React.Component {
         xmlns='http://www.w3.org/2000/svg'
         className={this.props.className || "layer"}
       >
-
-        <g transform='translate(10,10)'>
-          <g transform='translate(5,5)'>
+        <g transform='translate(45,0)'>
+          <g transform="rotate(10)">
             <Key
               x={1}
               y={33.4}
@@ -265,232 +264,235 @@ class Keymap extends React.Component {
               active={isActive(3, 5)}
               onClick={this.props.onKeySelect}
             />
+          </g>
 
+          <g transform="rotate(-10)">
+            <g transform="translate(0, 114)">
+              <Key
+                x={392.5}
+                y={33.4}
+                row={0}
+                col={7}
+                layer={this.props.index}
+                label={getLabel(0, 7)}
+                active={isActive(0, 7)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={446.5}
+                y={19.9}
+                row={0}
+                col={8}
+                layer={this.props.index}
+                label={getLabel(0, 8)}
+                active={isActive(0, 8)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={500.5}
+                y={1}
+                row={0}
+                col={9}
+                layer={this.props.index}
+                label={getLabel(0, 9)}
+                active={isActive(0, 9)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={554.5}
+                y={19.9}
+                row={0}
+                col={10}
+                layer={this.props.index}
+                label={getLabel(0, 10)}
+                active={isActive(0, 10)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={608.5}
+                y={33.4}
+                row={0}
+                col={11}
+                layer={this.props.index}
+                label={getLabel(0, 11)}
+                active={isActive(0, 11)}
+                onClick={this.props.onKeySelect}
+              />
 
-            <Key
-              x={392.5}
-              y={33.4}
-              row={0}
-              col={7}
-              layer={this.props.index}
-              label={getLabel(0, 7)}
-              active={isActive(0, 7)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={446.5}
-              y={19.9}
-              row={0}
-              col={8}
-              layer={this.props.index}
-              label={getLabel(0, 8)}
-              active={isActive(0, 8)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={500.5}
-              y={1}
-              row={0}
-              col={9}
-              layer={this.props.index}
-              label={getLabel(0, 9)}
-              active={isActive(0, 9)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={554.5}
-              y={19.9}
-              row={0}
-              col={10}
-              layer={this.props.index}
-              label={getLabel(0, 10)}
-              active={isActive(0, 10)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={608.5}
-              y={33.4}
-              row={0}
-              col={11}
-              layer={this.props.index}
-              label={getLabel(0, 11)}
-              active={isActive(0, 11)}
-              onClick={this.props.onKeySelect}
-            />
+              <Key
+                x={392.5}
+                y={87.4}
+                row={1}
+                col={7}
+                layer={this.props.index}
+                label={getLabel(1, 7)}
+                active={isActive(1, 7)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={446.5}
+                y={73.9}
+                row={1}
+                col={8}
+                layer={this.props.index}
+                label={getLabel(1, 8)}
+                active={isActive(1, 8)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={500.5}
+                y={55}
+                row={1}
+                col={9}
+                layer={this.props.index}
+                label={getLabel(1, 9)}
+                active={isActive(1, 9)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={554.5}
+                y={73.9}
+                row={1}
+                col={10}
+                layer={this.props.index}
+                label={getLabel(1, 10)}
+                active={isActive(1, 10)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={608.5}
+                y={87.4}
+                row={1}
+                col={11}
+                layer={this.props.index}
+                label={getLabel(1, 11)}
+                active={isActive(1, 11)}
+                onClick={this.props.onKeySelect}
+              />
 
-            <Key
-              x={392.5}
-              y={87.4}
-              row={1}
-              col={7}
-              layer={this.props.index}
-              label={getLabel(1, 7)}
-              active={isActive(1, 7)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={446.5}
-              y={73.9}
-              row={1}
-              col={8}
-              layer={this.props.index}
-              label={getLabel(1, 8)}
-              active={isActive(1, 8)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={500.5}
-              y={55}
-              row={1}
-              col={9}
-              layer={this.props.index}
-              label={getLabel(1, 9)}
-              active={isActive(1, 9)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={554.5}
-              y={73.9}
-              row={1}
-              col={10}
-              layer={this.props.index}
-              label={getLabel(1, 10)}
-              active={isActive(1, 10)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={608.5}
-              y={87.4}
-              row={1}
-              col={11}
-              layer={this.props.index}
-              label={getLabel(1, 11)}
-              active={isActive(1, 11)}
-              onClick={this.props.onKeySelect}
-            />
+              <Key
+                x={392.5}
+                y={141.4}
+                row={2}
+                col={7}
+                layer={this.props.index}
+                label={getLabel(2, 7)}
+                active={isActive(2, 7)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={446.5}
+                y={127.9}
+                row={2}
+                col={8}
+                layer={this.props.index}
+                label={getLabel(2, 8)}
+                active={isActive(2, 8)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={500.5}
+                y={109}
+                row={2}
+                col={9}
+                layer={this.props.index}
+                label={getLabel(2, 9)}
+                active={isActive(2, 9)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={554.5}
+                y={127.9}
+                row={2}
+                col={10}
+                layer={this.props.index}
+                label={getLabel(2, 10)}
+                active={isActive(2, 10)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={608.5}
+                y={141.4}
+                row={2}
+                col={11}
+                layer={this.props.index}
+                label={getLabel(2, 11)}
+                active={isActive(2, 11)}
+                onClick={this.props.onKeySelect}
+              />
 
-            <Key
-              x={392.5}
-              y={141.4}
-              row={2}
-              col={7}
-              layer={this.props.index}
-              label={getLabel(2, 7)}
-              active={isActive(2, 7)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={446.5}
-              y={127.9}
-              row={2}
-              col={8}
-              layer={this.props.index}
-              label={getLabel(2, 8)}
-              active={isActive(2, 8)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={500.5}
-              y={109}
-              row={2}
-              col={9}
-              layer={this.props.index}
-              label={getLabel(2, 9)}
-              active={isActive(2, 9)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={554.5}
-              y={127.9}
-              row={2}
-              col={10}
-              layer={this.props.index}
-              label={getLabel(2, 10)}
-              active={isActive(2, 10)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={608.5}
-              y={141.4}
-              row={2}
-              col={11}
-              layer={this.props.index}
-              label={getLabel(2, 11)}
-              active={isActive(2, 11)}
-              onClick={this.props.onKeySelect}
-            />
+              <Key
+                x={392.5}
+                y={195.4}
+                row={3}
+                col={7}
+                layer={this.props.index}
+                label={getLabel(3, 7)}
+                active={isActive(3, 7)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={446.5}
+                y={181.9}
+                row={3}
+                col={8}
+                layer={this.props.index}
+                label={getLabel(3, 8)}
+                active={isActive(3, 8)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={500.5}
+                y={163}
+                row={3}
+                col={9}
+                layer={this.props.index}
+                label={getLabel(3, 9)}
+                active={isActive(3, 9)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={554.5}
+                y={181.9}
+                row={3}
+                col={10}
+                layer={this.props.index}
+                label={getLabel(3, 10)}
+                active={isActive(3, 10)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={608.5}
+                y={195.4}
+                row={3}
+                col={11}
+                layer={this.props.index}
+                label={getLabel(3, 11)}
+                active={isActive(3, 11)}
+                onClick={this.props.onKeySelect}
+              />
 
-            <Key
-              x={392.5}
-              y={195.4}
-              row={3}
-              col={7}
-              layer={this.props.index}
-              label={getLabel(3, 7)}
-              active={isActive(3, 7)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={446.5}
-              y={181.9}
-              row={3}
-              col={8}
-              layer={this.props.index}
-              label={getLabel(3, 8)}
-              active={isActive(3, 8)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={500.5}
-              y={163}
-              row={3}
-              col={9}
-              layer={this.props.index}
-              label={getLabel(3, 9)}
-              active={isActive(3, 9)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={554.5}
-              y={181.9}
-              row={3}
-              col={10}
-              layer={this.props.index}
-              label={getLabel(3, 10)}
-              active={isActive(3, 10)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={608.5}
-              y={195.4}
-              row={3}
-              col={11}
-              layer={this.props.index}
-              label={getLabel(3, 11)}
-              active={isActive(3, 11)}
-              onClick={this.props.onKeySelect}
-            />
-
-            <Key
-              x={338.5}
-              y={127.9}
-              row={2}
-              col={6}
-              layer={this.props.index}
-              label={getLabel(2, 6)}
-              active={isActive(2, 6)}
-              onClick={this.props.onKeySelect}
-            />
-            <Key
-              x={338.5}
-              y={181.9}
-              row={3}
-              col={6}
-              layer={this.props.index}
-              label={getLabel(3, 6)}
-              active={isActive(3, 6)}
-              onClick={this.props.onKeySelect}
-            />
+              <Key
+                x={338.5}
+                y={127.9}
+                row={2}
+                col={6}
+                layer={this.props.index}
+                label={getLabel(2, 6)}
+                active={isActive(2, 6)}
+                onClick={this.props.onKeySelect}
+              />
+              <Key
+                x={338.5}
+                y={181.9}
+                row={3}
+                col={6}
+                layer={this.props.index}
+                label={getLabel(3, 6)}
+                active={isActive(3, 6)}
+                onClick={this.props.onKeySelect}
+              />
+            </g>
           </g>
         </g>
       </svg>

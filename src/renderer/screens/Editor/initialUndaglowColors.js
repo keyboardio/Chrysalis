d74c7a91ac6bcbad508aed33f75cb46c951d4e4f
Roman Danilov <r.danylov@gmail.com>
2019-12-08 22:12:36 +0200

    Fix: undeglow dont save at different layers

---
+++ src/renderer/screens/Editor/initialUndaglowColors.js
@@ -0,0 +1,12 @@
+export const undeglowDefaultColors = {
+  0: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" },
+  1: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" },
+  2: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" },
+  3: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" },
+  4: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" },
+  5: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" },
+  6: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" },
+  7: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" },
+  8: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" },
+  9: { r: 0, g: 0, b: 0, rgb: "rgb(0, 0, 0)" }
+};

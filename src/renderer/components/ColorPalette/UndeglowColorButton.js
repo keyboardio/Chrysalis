1d4ed965f1466b1e703818795771a92167748743
Gergely Nagy <me@gergo.csillger.hu>
2019-12-17 19:03:34 +0100

    Update a few Chrysalis references to use Bazecor instead
    This is mostly in file headers.
    
    Signed-off-by: Gergely Nagy <me@gergo.csillger.hu>

--- src/renderer/components/ColorPalette/UndeglowColorButton.js
+++ src/renderer/components/ColorPalette/UndeglowColorButton.js
@@ -1,5 +1,5 @@
 // -*- mode: js-jsx -*-
-/* Chrysalis -- Kaleidoscope Command Center
+/* Bazecor -- Kaleidoscope Command Center
  * Copyright (C) 2019  DygmaLab SE
  *
  * This program is free software: you can redistribute it and/or modify it under

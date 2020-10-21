cab2705b5ae56724f8cfce2084a5613ccdc9b163
AlexDygma <alex@dygma.com>
2020-10-21 09:16:35 +0200

    Added drag & drop fu nctionality to table interface

--- src/renderer/components/MacroManager/MacroForm.js
+++ src/renderer/components/MacroManager/MacroForm.js
@@ -1,27 +1,10 @@
 import React, { Component } from "react";
 import classNames from "classnames";
+import MacroTable from "./MacroTable";
 
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import IconButton from "@material-ui/core/IconButton";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import FormControl from "@material-ui/core/FormControl";
-import Paper from "@material-ui/core/Paper";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import AddCircle from "@material-ui/icons/AddCircle";
-import CloseIcon from "@material-ui/icons/Close";
-// import ArrowDownward from "@material-ui/icons/ArrowDownward";
-// https://codesandbox.io/s/4qp6vjp319?file=/index.js
 
 const styles = theme => ({
   root: {
@@ -42,12 +25,6 @@ const styles = theme => ({
   },
   buttonAdd: {
     marginLeft: "25%"
-  },
-  table: {
-    minWidth: 700
-  },
-  select: {
-    minWidth: "100px"
   }
 });
 
@@ -88,62 +65,6 @@ class MacroForm extends Component {
 
   render() {
     const { classes, close, addMacro, macros, maxMacros } = this.props;
-    const rows = [
-      { symbol: "h", action: 8, id: 0 },
-      { symbol: "o", action: 8, id: 1 },
-      { symbol: "l", action: 8, id: 2 },
-      { symbol: "a", action: 8, id: 3 }
-    ];
-    const modifiers = [
-      { id: 0, name: "Shift" },
-      { id: 1, name: "Control" },
-      { id: 2, name: "Alt" },
-      { id: 3, name: "AltGr" },
-      { id: 4, name: "Gui" }
-    ];
-    const selectItems = modifiers.map(item => (
-      <MenuItem value={item.id} key={`item-${item.id}`}>
-        <div style={{ display: "flex" }}>
-          <ListItemText inset primary={item.name} />
-        </div>
-      </MenuItem>
-    ));
-    const actions = [
-      { enum: "MACRO_ACTION_END", id: 0, name: "End macro" },
-      { enum: "MACRO_ACTION_STEP_INTERVAL", id: 1, name: "Step Interval" },
-      { enum: "MACRO_ACTION_STEP_WAIT", id: 2, name: "Step Wait" },
-      { enum: "MACRO_ACTION_STEP_KEYDOWN", id: 3, name: "Step Keydown" },
-      { enum: "MACRO_ACTION_STEP_KEYUP", id: 4, name: "Step KeyUp" },
-      { enum: "MACRO_ACTION_STEP_TAP", id: 5, name: "Step Tap" },
-      {
-        enum: "MACRO_ACTION_STEP_KEYCODEDOWN",
-        id: 6,
-        name: "Step KeyCode Down"
-      },
-      { enum: "MACRO_ACTION_STEP_KEYCODEUP", id: 7, name: "Step KeyCode Up" },
-      { enum: "MACRO_ACTION_STEP_TAPCODE", id: 8, name: "Step Tap Code" },
-      {
-        enum: "MACRO_ACTION_STEP_EXPLICIT_REPORT",
-        id: 9,
-        name: "Explicit Report"
-      },
-      {
-        enum: "MACRO_ACTION_STEP_IMPLICIT_REPORT",
-        id: 10,
-        name: "Implicit Report"
-      },
-      { enum: "MACRO_ACTION_STEP_SEND_REPORT", id: 11, name: "Send Report" },
-      {
-        enum: "MACRO_ACTION_STEP_TAP_SEQUENCE",
-        id: 12,
-        name: "Step Tap Sequence"
-      },
-      {
-        enum: "MACRO_ACTION_STEP_TAP_CODE_SEQUENCE",
-        id: 13,
-        name: "Step Code Sequence"
-      }
-    ];
     return (
       <React.Fragment>
         <div>
@@ -157,76 +78,7 @@ class MacroForm extends Component {
               this.setState({ name: e.target.value });
             }}
           />
-          <div>
-            <InputLabel
-              className={classNames(
-                classes.margin,
-                classes.textField,
-                classes.code
-              )}
-            >
-              Add text to Macro
-            </InputLabel>
-            <Input
-              id="AddToMacro"
-              className={classNames(
-                classes.margin,
-                classes.textField,
-                classes.code
-              )}
-              multiline
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton>
-                    <AddCircle />
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </div>
-          <Paper className={classes.root}>
-            <Table className={classes.table}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Symbol</TableCell>
-                  <TableCell align="right">Action Type</TableCell>
-                  <TableCell align="right" />
-                  <TableCell align="right" />
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map(row => (
-                  <TableRow key={row.id}>
-                    <TableCell component="th" scope="row">
-                      {row.symbol}
-                    </TableCell>
-                    <TableCell align="right">{row.action}</TableCell>
-                    <TableCell align="right">
-                      <Select
-                        className={classNames(classes.select)}
-                        placeholder="Add modifier"
-                      >
-                        {selectItems}
-                      </Select>
-                      <Button
-                        variant="outlined"
-                        color="primary"
-                        className={classes.margin}
-                        onClick={close}
-                      >
-                        {"Add"}
-                      </Button>
-                    </TableCell>
-                    <TableCell align="right">
-                      <IconButton color="secondary" variant="outlined">
-                        <CloseIcon />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Paper>
+          <MacroTable />
           <div>
             <Button
               variant="outlined"

4a2c4be4b83dbe48a8c9d3020eabdfa4d7fe72ea
Gergely Nagy <algernon@keyboard.io>
2021-01-25 13:51:01 +0100

    Fix the installation of the udev rules
    When installing the udev rules from the AppImage, we need to copy the rules to a
    temporary place first, before elevating privileges, and then copy from there to
    the final place. We need to do this because the AppImage temporary mount is
    locked down, and not even root can access it, so we need to get the source file
    out of there first.
    
    Fixes #603.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/renderer/utils/installUdevRules.js
+++ src/renderer/utils/installUdevRules.js
@@ -17,23 +17,33 @@
 
 import sudo from "sudo-prompt";
 import path from "path";
+import tmp from "tmp";
+import { spawn } from "child_process";
 import { getStaticPath } from "../config";
 
 import Log from "../../api/log";
 
 const installUdevRules = async devicePath => {
   const rules = path.join(getStaticPath(), "udev", "60-kaleidoscope.rules");
+  const tmpRules = tmp.fileSync();
+  const logger = new Log();
+
+  logger.debug("Running: ", "cp " + rules + " " + tmpRules.name);
+  await spawn("cp", [rules, tmpRules.name]);
+
   const cmd =
     "(mv /etc/udev/rules.d/60-kaleidoscope.rules " +
     "/etc/udev/rules.d/60-kaleidoscope.rules.orig || true) && " +
-    "cp " +
-    rules +
-    " /etc/udev/rules.d/ && " +
+    "install -o root -g root " +
+    tmpRules.name +
+    " /etc/udev/rules.d/60-kaleidoscope.rules && " +
+    "rm " +
+    tmpRules.name +
+    " && " +
     "udevadm control --reload-rules && " +
     "udevadm trigger " +
     devicePath;
   return new Promise(async (resolve, reject) => {
-    let logger = new Log();
     logger.debug("Running:", cmd);
     sudo.exec(cmd, { name: "Chrysalis" }, error => {
       if (error) {

9fe6a8832af6be50161ed794a74dc152d113d716
Caleb Maclennan <caleb@alerque.com>
2021-04-10 12:00:20 +0300

    Refactor bash launcher script with less convoluted tricks
    Signed-off-by: Caleb Maclennan <caleb@alerque.com>

--- build/launcher.sh
+++ build/launcher.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env bash
 set -e
 
-if [ ! -z "$WAYLAND_DISPLAY" ]; then
-    WAYLAND_OPTIONS="--enable-features=UseOzonePlatform --ozone-platform=wayland"
+if [[ -n "$WAYLAND_DISPLAY" ]]; then
+	set -- --enable-features=UseOzonePlatform --ozone-platform=wayland "$@"
 fi
 
-UNPRIVILEGED_USERNS_ENABLED=$(cat /proc/sys/kernel/unprivileged_userns_clone 2>/dev/null || echo 0)
+if ! grep -qFx 1 /proc/sys/kernel/unprivileged_userns_clone; then
+	set -- --no-sandbox "$@"
+fi
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-exec "$SCRIPT_DIR/chrysalis-bin" "$([[ $UNPRIVILEGED_USERNS_ENABLED == 0 ]] && echo '--no-sandbox')" "${WAYLAND_OPTIONS}" "$@"
+
+exec "$SCRIPT_DIR/chrysalis-bin" "$@"

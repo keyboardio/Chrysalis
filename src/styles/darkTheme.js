668d31f3ede7578bd405987152cf675a4ae7a8e9
Jesse Vincent <jesse@keyboard.io>
2022-03-22 21:37:28 -0700

    Fix for a backward incompatible change in Material UI

--- src/styles/darkTheme.js
+++ src/styles/darkTheme.js
@@ -1,6 +1,6 @@
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
 
-export const darkTheme = createMuiTheme({
+export const darkTheme = createTheme({
   palette: {
     type: "dark"
   },

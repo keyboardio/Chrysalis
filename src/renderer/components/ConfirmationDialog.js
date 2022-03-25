26a76a59af08d6fd92d7f5d46318f7eff47385e3
Jesse Vincent <jesse@keyboard.io>
2022-03-25 12:55:04 -0700

    mui5 changes for confirmation dialog

--- src/renderer/components/ConfirmationDialog.js
+++ src/renderer/components/ConfirmationDialog.js
@@ -17,11 +17,11 @@
 
 import React from "react";
 
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 import i18n from "../i18n";
 

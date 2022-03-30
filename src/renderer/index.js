6a53d652ee1ee474c75024dc62ab8b2f9c8ad3d4
Jesse Vincent <jesse@keyboard.io>
2022-03-30 12:34:44 -0700

    We were missing the mandatory configuration for hot module reload to work in dev
    Signed-off-by: Jesse Vincent <jesse@keyboard.io>

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -26,6 +26,9 @@ import "../styles/keymap.css";
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
 
+// Enable Hot Module Reload in dev
+if (module.hot) module.hot.accept();
+
 try {
   ReactDOM.render(
     <I18nextProvider i18n={i18n}>

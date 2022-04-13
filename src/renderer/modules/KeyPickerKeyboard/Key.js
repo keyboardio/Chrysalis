7205c7e956c058d27c56f918886676be8106761e
Thiago Hernandez <thiago.hernandez@gmail.com>
2022-04-13 17:16:25 +0200

    Fixed keyboard layout in Spanish

--- src/renderer/modules/KeyPickerKeyboard/Key.js
+++ src/renderer/modules/KeyPickerKeyboard/Key.js
@@ -338,13 +338,13 @@ class Key extends React.Component {
             <>
               <g filter={`url(#filter0_d_2211_181319)`}>
                 <path
-                  d="M0 3a3 3 0 013-3h35a3 3 0 013 3v60a3 3 0 01-3 3H14a3 3 0 01-3-3V33a3 3 0 00-3-3H3a3 3 0 01-3-3V3z"
+                  d="M0 3a3 3 0 013-3h38a3 3 0 013 3v60a3 3 0 01-3 3H14a3 3 0 01-3-3V33a3 3 0 00-3-3H3a3 3 0 01-3-3V3z"
                   className="baseKey KeyPositionEnter"
                   onClick={clicked}
                 />
               </g>
               <path
-                d="M0 3a3 3 0 013-3h35a3 3 0 013 3v60a3 3 0 01-3 3H14a3 3 0 01-3-3V33a3 3 0 00-3-3H3a3 3 0 01-3-3V3z"
+                d="M0 3a3 3 0 013-3h38a3 3 0 013 3v60a3 3 0 01-3 3H14a3 3 0 01-3-3V33a3 3 0 00-3-3H3a3 3 0 01-3-3V3z"
                 fill={`url(#paint_gradient)`}
                 fillOpacity={0.1}
                 className="shapeKey KeyPositionEnter"

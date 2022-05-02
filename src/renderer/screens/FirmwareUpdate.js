ca304ef0490ca9b2e120953be5244209b3634531
Gergely Nagy <algernon@keyboard.io>
2022-05-02 13:47:41 +0200

    screens/FirmwareUpdate: Fix syntax & style errors
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/renderer/screens/FirmwareUpdate.js
+++ src/renderer/screens/FirmwareUpdate.js
@@ -114,17 +114,27 @@ class FirmwareUpdate extends React.Component {
 
   _defaultFirmwareFilename = () => {
     const { vendor, product } = this.state.device.info;
-    const firmwareType = thi.state.device.info.firmwareType || "hex";
+    const firmwareType = this.state.device.info.firmwareType || "hex";
     const cVendor = vendor.replace("/", ""),
       cProduct = product.replace("/", "");
-    return path.join(getStaticPath(), cVendor, cProduct, "default." + firmwareType);
+    return path.join(
+      getStaticPath(),
+      cVendor,
+      cProduct,
+      "default." + firmwareType
+    );
   };
   _experimentalFirmwareFilename = () => {
     const { vendor, product } = this.state.device.info;
-    const firmwareType = thi.state.device.info.firmwareType || "hex";
+    const firmwareType = this.state.device.info.firmwareType || "hex";
     const cVendor = vendor.replace("/", ""),
       cProduct = product.replace("/", "");
-    return path.join(getStaticPath(), cVendor, cProduct, "experimental" + firmwareType);
+    return path.join(
+      getStaticPath(),
+      cVendor,
+      cProduct,
+      "experimental" + firmwareType
+    );
   };
 
   _flash = async () => {

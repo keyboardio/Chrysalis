ff56c6dab1fd9f57eb68465b6343e8f5e323f23e
Thiago Hernandez <thiago.hernandez@gmail.com>
2022-05-06 10:41:59 +0200

    Re-add the right modifiers to macro editor view

--- src/renderer/modules/Macros/TimelineEditorMacroTable.js
+++ src/renderer/modules/Macros/TimelineEditorMacroTable.js
@@ -76,13 +76,13 @@ class TimelineEditorMacroTable extends Component {
     this.keymapDB = props.keymapDB;
     this.modifiers = [
       { name: "LEFT SHIFT", keyCode: 225, color: "#e1f3f7" },
-      // { name: "RIGHT SHIFT", keyCode: 229, color: "#e1f3f7" },
+      { name: "RIGHT SHIFT", keyCode: 229, color: "#e1f3f7" },
       { name: "LEFT CTRL", keyCode: 224, color: "#f5e4e4" },
-      // { name: "RIGHT CTRL", keyCode: 228, color: "#f5e4e4" },
+      { name: "RIGHT CTRL", keyCode: 228, color: "#f5e4e4" },
       { name: "LEFT ALT", keyCode: 226, color: "#faf8e1" },
       { name: "RIGHT ALT", keyCode: 230, color: "#f2e7f5" },
-      { name: "LEFT GUI", keyCode: 227, color: "#e6f0e4" }
-      // { name: "RIGHT GUI", keyCode: 231, color: "#e6f0e4" }
+      { name: "LEFT GUI", keyCode: 227, color: "#e6f0e4" },
+      { name: "RIGHT GUI", keyCode: 231, color: "#e6f0e4" }
     ];
     this.actionTypes = [
       {

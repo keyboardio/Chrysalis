5584e081da3570e9d97ae38731164d3e6477acf5
Jesse Vincent <jesse@keyboard.io>
2022-05-10 20:00:23 -0700

    Remove all of the tiny little 'foo menu item' items as a first step toward making the code simpler

--- src/renderer/components/MainMenu/MainMenu.js
+++ src/renderer/components/MainMenu/MainMenu.js
@@ -25,22 +25,23 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListSubheader from "@mui/material/ListSubheader";
+import ListItemIcon from "@mui/material/ListItemIcon";
 
 import logo from "../../logo-small.png";
 import i18n from "../../i18n";
 
 import { version } from "../../../../package.json";
-import WelcomeMenu from "./WelcomeMenu";
-import SystemInfoMenuItem from "./SystemInfoMenuItem";
-import EditorMenuItem from "./EditorMenuItem";
-import FlashMenuItem from "./FlashMenuItem";
-import ChatMenuItem from "./ChatMenuItem";
-import ExitMenuItem from "./ExitMenuItem";
-import KeyboardMenuItem from "./KeyboardSelectMenuItem";
-import PreferencesMenuItem from "./PreferencesMenuItem";
+
 import UpgradeMenuItem from "./UpgradeMenuItem";
-import ChangeLogMenuItem from "./ChangeLogMenuItem";
 import openURL from "../../utils/openURL";
+import ChatIcon from "@mui/icons-material/Chat";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import InfoIcon from "@mui/icons-material/Info";
+import ListIcon from "@mui/icons-material/List";
+import SettingsIcon from "@mui/icons-material/Settings";
+import KeyboardIcon from "@mui/icons-material/Keyboard";
+
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 import { history } from "../../routerHistory";
 
@@ -90,21 +91,39 @@ function MainMenu({ open, closeMenu, classes, connected, pages }) {
           }
         >
           {!pages.keymap && !pages.colormap && (
-            <WelcomeMenu
+            <ListItem
+              button
               selected={currentPage == "/welcome"}
               onClick={() => setCurrentPage("/welcome")}
-            />
+            >
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary={i18n.t("app.menu.welcome")} />
+            </ListItem>
           )}
           {pages.keymap && (
-            <EditorMenuItem
+            <ListItem
+              button
               selected={currentPage == "/editor"}
               onClick={() => setCurrentPage("/editor")}
-            />
+            >
+              <ListItemIcon>
+                <KeyboardIcon />
+              </ListItemIcon>
+              <ListItemText primary={i18n.t("app.menu.editor")} />
+            </ListItem>
           )}
-          <FlashMenuItem
+          <ListItem
+            button
             selected={currentPage == "/firmware-update"}
             onClick={() => setCurrentPage("/firmware-update")}
-          />
+          >
+            <ListItemIcon>
+              <CloudUploadIcon />
+            </ListItemIcon>
+            <ListItemText primary={i18n.t("app.menu.firmwareUpdate")} />
+          </ListItem>
         </List>
       )}
       {connected && <Divider />}
@@ -115,19 +134,33 @@ function MainMenu({ open, closeMenu, classes, connected, pages }) {
           </ListSubheader>
         }
       >
-        <KeyboardMenuItem
-          keyboardSelectText={
-            connected
-              ? i18n.t("app.menu.selectAnotherKeyboard")
-              : i18n.t("app.menu.selectAKeyboard")
-          }
+        <ListItem
+          button
           selected={currentPage == "/keyboard-select"}
           onClick={() => setCurrentPage("/keyboard-select")}
-        />
-        <PreferencesMenuItem
+        >
+          <ListItemIcon>
+            <KeyboardIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary={
+              connected
+                ? i18n.t("app.menu.selectAnotherKeyboard")
+                : i18n.t("app.menu.selectAKeyboard")
+            }
+          />
+        </ListItem>
+
+        <ListItem
+          button
           selected={currentPage == "/preferences"}
           onClick={() => setCurrentPage("/preferences")}
-        />
+        >
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary={i18n.t("app.menu.preferences")} />
+        </ListItem>
       </List>
       <Divider />
       <List
@@ -137,18 +170,43 @@ function MainMenu({ open, closeMenu, classes, connected, pages }) {
           </ListSubheader>
         }
       >
-        <ChatMenuItem
+        <ListItem
+          button
           onClick={() => openExternalPage("https://keyboard.io/discord-invite")}
-        />
-        <SystemInfoMenuItem
+        >
+          <ListItemIcon>
+            <ChatIcon />
+          </ListItemIcon>
+          <ListItemText primary={i18n.t("app.menu.chat")} />
+        </ListItem>
+
+        <ListItem
+          button
           selected={currentPage == "/system-info"}
           onClick={() => setCurrentPage("/system-info")}
-        />
-        <ChangeLogMenuItem
+        >
+          <ListItemIcon>
+            <InfoIcon />
+          </ListItemIcon>
+          <ListItemText primary={i18n.t("app.menu.systemInfo")} />
+        </ListItem>
+
+        <ListItem
+          button
           selected={currentPage == "/changelog"}
           onClick={() => setCurrentPage("/changelog")}
-        />
-        <ExitMenuItem onClick={() => ipcRenderer.send("app-exit")} />
+        >
+          <ListItemIcon>
+            <ListIcon />
+          </ListItemIcon>
+          <ListItemText primary={i18n.t("app.menu.changelog")} />
+        </ListItem>
+        <ListItem button onClick={() => ipcRenderer.send("app-exit")}>
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText primary={i18n.t("app.menu.exit")} />
+        </ListItem>
       </List>
       <Divider />
       <List>

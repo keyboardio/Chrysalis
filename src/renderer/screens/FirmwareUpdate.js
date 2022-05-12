a7931e47bf5a5bd9e9a58078892a70417072a110
Jesse Vincent <jesse@keyboard.io>
2022-05-11 22:45:42 -0700

    Convert 'FirmwareUpdate' to a functional component

--- src/renderer/screens/FirmwareUpdate.js
+++ src/renderer/screens/FirmwareUpdate.js
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from "react";
+import React, { useState } from "react";
 import { Electron, ipcRenderer } from "electron";
 import path from "path";
 import fs from "fs";
@@ -60,39 +60,31 @@ import clearEEPROM from "../utils/clearEEPROM";
 import checkExternalFlasher from "../utils/checkExternalFlasher";
 import { PageTitle } from "../components/PageTitle";
 
-class FirmwareUpdate extends React.Component {
-  constructor(props) {
-    super(props);
+const FirmwareUpdate = (props) => {
+  let focus = new Focus();
 
-    let focus = new Focus();
+  const [firmwareFilename, setFirmwareFilename] = useState("");
+  const [selected, setSelected] = useState("default");
+  const [device, setDevice] = useState(props.device || focus.device);
+  const [confirmationOpen, setConfirmationOpen] = useState(false);
+  const [resetOnFlash, setResetOnFlash] = useState(false);
+  const [activeStep, setActiveStep] = useState(-1);
 
-    this.state = {
-      firmwareFilename: "",
-      selected: "default",
-      device: props.device || focus.device,
-      confirmationOpen: false,
-      resetOnFlash: false,
-      activeStep: -1,
-    };
-  }
-
-  setResetOnFlash = (event) => {
-    this.setState({
-      resetOnFlash: event.target.checked,
-    });
+  const toggleResetOnFlash = (event) => {
+    setResetOnFlash(event.target.checked);
   };
 
-  openConfirmationDialog = () => {
-    this.setState({ confirmationOpen: true });
+  const openConfirmationDialog = () => {
+    setConfirmationOpen(true);
   };
-  closeConfirmationDialog = () => {
-    this.setState({ confirmationOpen: false });
+  const closeConfirmationDialog = () => {
+    setConfirmationOpen(false);
   };
 
-  selectFirmware = (event) => {
-    this.setState({ selected: event.target.value });
+  const selectFirmware = (event) => {
+    setSelected(event.target.value);
     if (event.target.value != "custom") {
-      return this.setState({ firmwareFilename: "" });
+      setFirmwareFilename("");
     }
 
     const [fileName, fileData] = ipcRenderer.sendSync("file-open", {
@@ -114,9 +106,9 @@ class FirmwareUpdate extends React.Component {
     }
   };
 
-  _defaultFirmwareFilename = () => {
-    const { vendor, product } = this.state.device.info;
-    const firmwareType = this.state.device.info.firmwareType || "hex";
+  const _defaultFirmwareFilename = () => {
+    const { vendor, product } = device.info;
+    const firmwareType = device.info.firmwareType || "hex";
     const cVendor = vendor.replace("/", ""),
       cProduct = product.replace("/", "");
     return path.join(
@@ -126,9 +118,9 @@ class FirmwareUpdate extends React.Component {
       "default." + firmwareType
     );
   };
-  _experimentalFirmwareFilename = () => {
-    const { vendor, product } = this.state.device.info;
-    const firmwareType = this.state.device.info.firmwareType || "hex";
+  const _experimentalFirmwareFilename = () => {
+    const { vendor, product } = device.info;
+    const firmwareType = device.info.firmwareType || "hex";
     const cVendor = vendor.replace("/", ""),
       cProduct = product.replace("/", "");
     return path.join(
@@ -139,50 +131,47 @@ class FirmwareUpdate extends React.Component {
     );
   };
 
-  _flash = async () => {
+  const _flash = async () => {
     let focus = new Focus();
     let filename;
 
-    if (this.state.selected == "default") {
-      filename = this._defaultFirmwareFilename();
-    } else if (this.state.selected == "experimental") {
-      filename = this._experimentalFirmwareFilename();
+    if (selected == "default") {
+      filename = _defaultFirmwareFilename();
+    } else if (selected == "experimental") {
+      filename = _experimentalFirmwareFilename();
     } else {
-      filename = this.state.firmwareFilename;
+      filename = firmwareFilename;
     }
 
-    const _this = this;
     const nextStep = async (desiredState) => {
-      return _this.setState((state) => {
-        let activeStep = state.activeStep + 1;
-        _this.state.device.flashSteps.forEach((step, index) => {
-          if (step == desiredState) activeStep = index;
-        });
-        return {
-          activeStep: activeStep,
-        };
+      setActiveStep(activeStep + 1);
+      device.flashSteps.forEach((step, index) => {
+        if (step == desiredState) {
+          setActiveStep(index);
+        }
       });
+      return {
+        activeStep: activeStep,
+      };
     };
 
     const preferExternalFlasher =
       (await settings.get("flash.preferExternalFlasher")) &&
-      (await checkExternalFlasher(this.state.device));
-    return this.state.device.flash(focus._port, filename, {
+      (await checkExternalFlasher(device));
+    return device.flash(focus._port, filename, {
       preferExternalFlasher: preferExternalFlasher,
-      device: this.state.device,
+      device: device,
       focus: focus,
       callback: nextStep,
     });
   };
 
-  upload = async () => {
-    await this.props.toggleFlashing();
+  const upload = async () => {
+    await props.toggleFlashing();
 
     try {
-      await this._flash();
-      await this.setState((state) => ({
-        activeStep: state.activeStep + 1,
-      }));
+      await _flash();
+      setActiveStep(activeStep + 1);
     } catch (e) {
       console.error(e);
       const action = ({ closeToast }) => (
@@ -201,12 +190,12 @@ class FirmwareUpdate extends React.Component {
           <Button onClick={closeToast}>Dismiss</Button>
         </React.Fragment>
       );
-      this.setState({ activeStep: -1 });
+      setActiveStep(-1);
       toast.error(i18n.t("firmwareUpdate.flashing.error"), {
         closeButton: action,
       });
-      this.props.toggleFlashing();
-      this.props.onDisconnect();
+      props.toggleFlashing();
+      props.onDisconnect();
       return;
     }
 
@@ -214,231 +203,219 @@ class FirmwareUpdate extends React.Component {
       setTimeout(() => {
         toast.success(i18n.t("firmwareUpdate.flashing.success"));
 
-        this.props.toggleFlashing();
-        this.props.onDisconnect();
+        props.toggleFlashing();
+        props.onDisconnect();
         resolve();
       }, 1000);
     });
   };
 
-  replace = async () => {
-    this.closeConfirmationDialog();
-    await this.setState({ activeState: 0 });
+  const replace = async () => {
+    closeConfirmationDialog();
+    setActiveStep(0);
     await clearEEPROM();
-    return await this.upload();
+    return await upload();
   };
 
-  render() {
-    const { firmwareFilename } = this.state;
+  let filename = null;
+  if (firmwareFilename) {
+    filename = firmwareFilename.split(/[\\/]/);
+    filename = filename[filename.length - 1];
+  }
 
-    let filename = null;
-    if (firmwareFilename) {
-      filename = firmwareFilename.split(/[\\/]/);
-      filename = filename[filename.length - 1];
-    }
-
-    const experimentalFirmwareItemText = i18n.t(
-      "firmwareUpdate.experimentalFirmware",
-      { version: version }
-    );
-    const experimentalFirmwareItem = (
-      <MenuItem
-        value="experimental"
-        selected={this.state.selected == "experimental"}
-      >
-        <ListItemIcon>
-          <ExploreIcon />
-        </ListItemIcon>
-        <ListItemText
-          primary={experimentalFirmwareItemText}
-          secondary={i18n.t("firmwareUpdate.experimentalFirmwareDescription")}
-        />
-      </MenuItem>
-    );
-    let hasExperimentalFirmware = true;
-
-    try {
-      fs.accessSync(this._experimentalFirmwareFilename(), fs.constants.R_OK);
-    } catch (_) {
-      hasExperimentalFirmware = false;
-    }
-
-    const defaultFirmwareItemText = i18n.t("firmwareUpdate.defaultFirmware", {
-      version: version,
-    });
-    const defaultFirmwareItem = (
-      <MenuItem value="default" selected={this.state.selected == "default"}>
-        <ListItemIcon>
-          <SettingsBackupRestoreIcon />
-        </ListItemIcon>
-        <ListItemText
-          primary={defaultFirmwareItemText}
-          secondary={
-            hasExperimentalFirmware &&
-            i18n.t("firmwareUpdate.defaultFirmwareDescription")
-          }
-        />
-      </MenuItem>
-    );
-    let hasDefaultFirmware = true;
-    try {
-      fs.accessSync(this._defaultFirmwareFilename(), fs.constants.R_OK);
-    } catch (_) {
-      hasDefaultFirmware = false;
-    }
-
-    const firmwareSelect = (
-      <FormControl
-        sx={{
-          marginLeft: 2,
-        }}
-      >
-        <InputLabel shrink htmlFor="selected-firmware">
-          {i18n.t("firmwareUpdate.selected")}
-        </InputLabel>
-        <Select
-          sx={{
-            display: "flex",
-            minWidth: "15em",
-          }}
-          value={this.state.selected}
-          input={<Input id="selected-firmware" />}
-          onChange={this.selectFirmware}
-        >
-          {hasDefaultFirmware && defaultFirmwareItem}
-          {hasExperimentalFirmware && experimentalFirmwareItem}
-          <MenuItem selected={this.state.selected == "custom"} value="custom">
-            <ListItemIcon
-              sx={{
-                marginTop: "auto",
-                marginBottom: "auto",
-              }}
-            >
-              <BuildIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary={i18n.t("firmwareUpdate.custom")}
-              secondary={filename}
-            />
-          </MenuItem>
-        </Select>
-      </FormControl>
-    );
-
-    const resetOnFlashSwitch = (
-      <Switch
-        checked={this.state.resetOnFlash}
-        value="resetOnFlash"
-        onClick={this.setResetOnFlash}
+  const experimentalFirmwareItemText = i18n.t(
+    "firmwareUpdate.experimentalFirmware",
+    { version: version }
+  );
+  const experimentalFirmwareItem = (
+    <MenuItem value="experimental" selected={selected == "experimental"}>
+      <ListItemIcon>
+        <ExploreIcon />
+      </ListItemIcon>
+      <ListItemText
+        primary={experimentalFirmwareItemText}
+        secondary={i18n.t("firmwareUpdate.experimentalFirmwareDescription")}
       />
-    );
+    </MenuItem>
+  );
+  let hasExperimentalFirmware = true;
 
-    let steps = [];
-    if (this.state.resetOnFlash) {
-      steps = ["factoryRestore"];
-    }
-    if (this.state.device.flashSteps) {
-      steps = steps.concat(this.state.device.flashSteps);
-    } else {
-      steps = steps.concat(["flash"]);
-    }
+  try {
+    fs.accessSync(_experimentalFirmwareFilename(), fs.constants.R_OK);
+  } catch (_) {
+    hasExperimentalFirmware = false;
+  }
 
-    steps = steps.map((step) => {
-      return i18n.t("firmwareUpdate.flashing.steps." + step);
-    });
+  const defaultFirmwareItemText = i18n.t("firmwareUpdate.defaultFirmware", {
+    version: version,
+  });
 
-    const stepsWidget = (
-      <Stepper activeStep={this.state.activeStep}>
-        {steps.map((label) => {
-          return (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-    );
+  const defaultFirmwareItem = (
+    <MenuItem value="default" selected={selected == "default"}>
+      <ListItemIcon>
+        <SettingsBackupRestoreIcon />
+      </ListItemIcon>
+      <ListItemText
+        primary={defaultFirmwareItemText}
+        secondary={
+          hasExperimentalFirmware &&
+          i18n.t("firmwareUpdate.defaultFirmwareDescription")
+        }
+      />
+    </MenuItem>
+  );
+  let hasDefaultFirmware = true;
+  try {
+    fs.accessSync(_defaultFirmwareFilename(), fs.constants.R_OK);
+  } catch (_) {
+    hasDefaultFirmware = false;
+  }
 
-    return (
-      <Box
+  const firmwareSelect = (
+    <FormControl
+      sx={{
+        marginLeft: 2,
+      }}
+    >
+      <InputLabel shrink htmlFor="selected-firmware">
+        {i18n.t("firmwareUpdate.selected")}
+      </InputLabel>
+      <Select
         sx={{
           display: "flex",
-          justifyContent: "center",
+          minWidth: "15em",
+        }}
+        value={selected}
+        input={<Input id="selected-firmware" />}
+        onChange={selectFirmware}
+      >
+        {hasDefaultFirmware && defaultFirmwareItem}
+        {hasExperimentalFirmware && experimentalFirmwareItem}
+        <MenuItem selected={selected == "custom"} value="custom">
+          <ListItemIcon
+            sx={{
+              marginTop: "auto",
+              marginBottom: "auto",
+            }}
+          >
+            <BuildIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary={i18n.t("firmwareUpdate.custom")}
+            secondary={filename}
+          />
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+
+  const resetOnFlashSwitch = (
+    <Switch
+      checked={resetOnFlash}
+      value="resetOnFlash"
+      onClick={toggleResetOnFlash}
+    />
+  );
+
+  let steps = [];
+  if (resetOnFlash) {
+    steps = ["factoryRestore"];
+  }
+  if (device.flashSteps) {
+    steps = steps.concat(device.flashSteps);
+  } else {
+    steps = steps.concat(["flash"]);
+  }
+
+  steps = steps.map((step) => {
+    return i18n.t("firmwareUpdate.flashing.steps." + step);
+  });
+
+  const stepsWidget = (
+    <Stepper activeStep={activeStep}>
+      {steps.map((label) => {
+        return (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        );
+      })}
+    </Stepper>
+  );
+
+  return (
+    <div
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <PageTitle title={i18n.t("app.menu.firmwareUpdate")} />
+      <Card
+        sx={{
+          my: 4,
+          mx: "auto",
+          maxWidth: "50%",
         }}
       >
-        <PageTitle title={i18n.t("app.menu.firmwareUpdate")} />
-        <Card
-          sx={{
-            my: 4,
-            mx: "auto",
-            maxWidth: "50%",
-          }}
-        >
-          <CardContent>
-            <Typography component="p" gutterBottom>
-              {i18n.t("firmwareUpdate.description")}
-            </Typography>
-            <Typography component="p" gutterBottom sx={{ textAlign: "center" }}>
-              <a href="https://github.com/keyboardio/Chrysalis-Firmware-Bundle#readme">
-                Chrysalis-Firmware-Bundle
-              </a>
-            </Typography>
-            <Typography component="p" gutterBottom>
-              {i18n.t("hardware.updateInstructions")}
-            </Typography>
-            <Typography component="p" gutterBottom>
-              {i18n.t("firmwareUpdate.postUpload")}
-            </Typography>
-          </CardContent>
-          <Divider variant="middle" />
-          <CardContent>
-            <Typography variant="subtitle1">
-              {i18n.t("firmwareUpdate.options.title")}
-            </Typography>
-            <FormControl sx={{ display: "block" }}>
-              <FormControlLabel
-                control={resetOnFlashSwitch}
-                sx={{
-                  flexGrow: 1,
-                  display: "flex",
-                  marginRight: 2,
-                }}
-                labelPlacement="end"
-                label={i18n.t("firmwareUpdate.options.onFlash")}
-              />
-            </FormControl>
-          </CardContent>
-          <Divider variant="middle" />
-          <CardActions>
-            {firmwareSelect}
-            <Box sx={{ flexGrow: 1 }} />
-            <SaveChangesButton
-              icon={<CloudUploadIcon />}
-              onClick={
-                this.state.resetOnFlash
-                  ? this.openConfirmationDialog
-                  : this.upload
-              }
-              successMessage={i18n.t("firmwareUpdate.flashing.buttonSuccess")}
-            >
-              {i18n.t("firmwareUpdate.flashing.button")}
-            </SaveChangesButton>
-          </CardActions>
-          {this.state.activeStep >= 0 && (
-            <CardContent>{stepsWidget}</CardContent>
-          )}
-        </Card>
-        <ConfirmationDialog
-          title={i18n.t("firmwareUpdate.confirmDialog.title")}
-          open={this.state.confirmationOpen}
-          onConfirm={this.replace}
-          onCancel={this.closeConfirmationDialog}
-        >
-          {i18n.t("firmwareUpdate.confirmDialog.contents")}
-        </ConfirmationDialog>
-      </Box>
-    );
-  }
-}
+        <CardContent>
+          <Typography component="p" gutterBottom>
+            {i18n.t("firmwareUpdate.description")}
+          </Typography>
+          <Typography component="p" gutterBottom sx={{ textAlign: "center" }}>
+            <a href="https://github.com/keyboardio/Chrysalis-Firmware-Bundle#readme">
+              Chrysalis-Firmware-Bundle
+            </a>
+          </Typography>
+          <Typography component="p" gutterBottom>
+            {i18n.t("hardware.updateInstructions")}
+          </Typography>
+          <Typography component="p" gutterBottom>
+            {i18n.t("firmwareUpdate.postUpload")}
+          </Typography>
+        </CardContent>
+        <Divider variant="middle" />
+        <CardContent>
+          <Typography variant="subtitle1">
+            {i18n.t("firmwareUpdate.options.title")}
+          </Typography>
+          <FormControl sx={{ display: "block" }}>
+            <FormControlLabel
+              control={resetOnFlashSwitch}
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                marginRight: 2,
+              }}
+              labelPlacement="end"
+              label={i18n.t("firmwareUpdate.options.onFlash")}
+            />
+          </FormControl>
+        </CardContent>
+        <Divider variant="middle" />
+        <CardActions>
+          {firmwareSelect}
+          <div sx={{ flexGrow: 1 }} />
+          <SaveChangesButton
+            icon={<CloudUploadIcon />}
+            onClick={resetOnFlash ? openConfirmationDialog : upload}
+            successMessage={i18n.t("firmwareUpdate.flashing.buttonSuccess")}
+          >
+            {i18n.t("firmwareUpdate.flashing.button")}
+          </SaveChangesButton>
+        </CardActions>
+        {activeStep >= 0 && <CardContent>{stepsWidget}</CardContent>}
+      </Card>
+      <ConfirmationDialog
+        title={i18n.t("firmwareUpdate.confirmDialog.title")}
+        open={confirmationOpen}
+        onConfirm={replace}
+        onCancel={closeConfirmationDialog}
+      >
+        {i18n.t("firmwareUpdate.confirmDialog.contents")}
+      </ConfirmationDialog>
+    </div>
+  );
+};
 
 export default FirmwareUpdate;

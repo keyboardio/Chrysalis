3ef844b3adf6a5514dfcd15ba75d767a558f0377
Jesse Vincent <jesse@keyboard.io>
2022-05-12 22:59:16 -0700

    we no longer need to surface 'commands' to App.js

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -147,13 +147,12 @@ const App = (props) => {
       i18n.refreshHardware(port.focusDeviceDescriptor);
 
       await navigate("/focus-not-detected");
-      return [];
+      return false;
     }
 
     logger.log("Connecting to", port.path);
     await focus.open(port.path, port.focusDeviceDescriptor);
 
-    let commands = [];
     // TODO: I'm not quite sure how to set activeDevice in a way that
     // I can access it in this context, since activeDevice is const
     const newActiveDevice = new ActiveDevice();
@@ -161,7 +160,6 @@ const App = (props) => {
 
     if (!port.focusDeviceDescriptor.bootloader) {
       logger.log("Probing for Focus support...");
-      commands = await newActiveDevice.focusCommands();
       focus.setLayerSize(focus.focusDeviceDescriptor);
     }
 
@@ -171,7 +169,7 @@ const App = (props) => {
     await navigate(
       newActiveDevice.focusDetected() ? "/editor" : "/focus-not-detected"
     );
-    return commands;
+    return true;
   };
 
   const onKeyboardDisconnect = async () => {

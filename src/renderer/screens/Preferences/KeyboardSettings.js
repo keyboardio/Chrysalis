09d10c8588611a1ac5dbd4ae11412e5696b370a2
Jesse Vincent <jesse@keyboard.io>
2022-05-13 20:05:36 -0700

    Convert KeyboardSettings to a functional component

--- src/renderer/screens/Preferences/KeyboardSettings.js
+++ src/renderer/screens/Preferences/KeyboardSettings.js
@@ -16,8 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
 
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
@@ -51,324 +50,301 @@ import {
 const Store = require("electron-store");
 const settings = new Store();
 
-class KeyboardSettings extends React.Component {
-  state = {
-    keymap: {
-      custom: [],
-      default: [],
-      onlyCustom: false,
-    },
-    ledBrightness: 255,
-    ledIdleTimeLimit: 0,
-    defaultLayer: 126,
-    modified: false,
-    working: false,
-  };
+const KeyboardSettings = (props) => {
+  const [keymap, setKeymap] = useState({
+    custom: [],
+    default: [],
+    onlyCustom: false,
+  });
+  const [defaultLayer, setDefaultLayer] = useState(126);
+  const [ledBrightness, setLedBrightness] = useState(255);
+  const [ledIdleTimeLimit, setLedIdleTimeLimit] = useState(0);
 
-  componentDidMount() {
-    this.context_bar_channel = new BroadcastChannel("context_bar");
+  const [modified, setModified] = useState(false);
+  const [working, setWorking] = useState(false);
+
+  useEffect(() => {
+    const context_bar_channel = new BroadcastChannel("context_bar");
 
     const focus = new Focus();
 
     focus.command("keymap").then((keymap) => {
-      this.setState({ keymap: keymap });
+      setKeymap(keymap);
     });
+
     focus.command("settings.defaultLayer").then((layer) => {
       layer = layer ? parseInt(layer) : 126;
-      this.setState({ defaultLayer: layer <= 126 ? layer : 126 });
+      setDefaultLayer(layer <= 126 ? layer : 126);
     });
     focus.command("led.brightness").then((brightness) => {
       brightness = brightness ? parseInt(brightness) : -1;
-      this.setState({ ledBrightness: brightness });
+      setLedBrightness(brightness);
     });
     focus.command("idleleds.time_limit").then((limit) => {
       limit = limit ? parseInt(limit) : -1;
-      this.setState({ ledIdleTimeLimit: limit });
+      setLedIdleTimeLimit(limit);
     });
 
-    this.context_bar_channel.onmessage = (event) => {
+    context_bar_channel.onmessage = (event) => {
       if (event.data === "changes-discarded") {
-        this.componentDidMount();
-
-        this.setState({ modified: false });
+        // XXX TODO .componentDidMount();
+        alert("HEY");
+        setModified(false);
       }
     };
-  }
+    return () => {
+      context_bar_channel.close();
+    };
+  });
 
-  componentWillUnmount() {
-    this.context_bar_channel.close();
-  }
+  const selectDefaultLayer = (event) => {
+    setDefaultLayer(event.target.value);
 
-  selectDefaultLayer = (event) => {
-    this.setState({
-      defaultLayer: event.target.value,
-      modified: true,
-    });
+    setModified(true);
     showContextBar();
   };
 
-  selectIdleLEDTime = (event) => {
-    this.setState({
-      ledIdleTimeLimit: event.target.value,
-      modified: true,
-    });
+  const selectIdleLEDTime = (event) => {
+    setLedIdleTimeLimit(event.target.value);
+    setModified(true);
     showContextBar();
   };
 
-  setBrightness = (event, value) => {
-    this.setState({
-      ledBrightness: value,
-      modified: true,
-    });
+  const setBrightness = (event, value) => {
+    setLedBrightness(value);
+    setModified(true);
     showContextBar();
   };
 
-  saveKeymapChanges = async () => {
+  const saveKeymapChanges = async () => {
     const focus = new Focus();
-
-    const { defaultLayer, ledBrightness, ledIdleTimeLimit } = this.state;
 
     await focus.command("settings.defaultLayer", defaultLayer);
     await focus.command("led.brightness", ledBrightness);
     if (ledIdleTimeLimit >= 0)
       await focus.command("idleleds.time_limit", ledIdleTimeLimit);
-    this.setState({ modified: false });
+    setModified(false);
     hideContextBar();
   };
 
-  render() {
-    const { keymap, defaultLayer, modified, ledBrightness, ledIdleTimeLimit } =
-      this.state;
-
-    const layers = keymap.custom.map((_, index) => {
-      return (
-        <MenuItem value={index} key={index}>
-          {i18n.t("components.layer", { index: index })}
-        </MenuItem>
-      );
-    });
-
+  const layers = keymap.custom.map((_, index) => {
     return (
-      <React.Fragment>
-        {this.state.working && <LinearProgress variant="query" />}
-        <Typography
-          variant="subtitle1"
-          component="h2"
-          sx={{
-            marginTop: 4,
-            marginBottom: 1,
-          }}
-        >
-          {i18n.t("keyboardSettings.keymap.title")}
-        </Typography>
-        <Card>
+      <MenuItem value={index} key={index}>
+        {i18n.t("components.layer", { index: index })}
+      </MenuItem>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      {working && <LinearProgress variant="query" />}
+      <Typography
+        variant="subtitle1"
+        component="h2"
+        sx={{
+          marginTop: 4,
+          marginBottom: 1,
+        }}
+      >
+        {i18n.t("keyboardSettings.keymap.title")}
+      </Typography>
+      <Card>
+        <CardContent>
+          <FormGroup
+            sx={{
+              display: "block",
+            }}
+          >
+            <FormControl variant="standard" fullWidth={true}>
+              <InputLabel>
+                {i18n.t("keyboardSettings.keymap.defaultLayer")}
+              </InputLabel>
+              <Select
+                onChange={selectDefaultLayer}
+                sx={{ mb: 2 }}
+                value={defaultLayer}
+                variant="filled"
+                input={<FilledInput sx={{}} />}
+              >
+                <MenuItem value={126}>
+                  {i18n.t("keyboardSettings.keymap.noDefault")}
+                </MenuItem>
+                {layers}
+              </Select>
+            </FormControl>
+
+            {ledIdleTimeLimit >= 0 && (
+              <FormControl variant="standard" fullWidth={true}>
+                <InputLabel>
+                  {i18n.t("keyboardSettings.led.idleTimeLimit")}
+                </InputLabel>
+                <Select
+                  onChange={selectIdleLEDTime}
+                  value={ledIdleTimeLimit}
+                  variant="filled"
+                  input={<FilledInput sx={{}} />}
+                >
+                  <MenuItem value={0}>
+                    {i18n.t("keyboardSettings.led.idleDisabled")}
+                  </MenuItem>
+                  <MenuItem value={60}>
+                    {i18n.t("keyboardSettings.led.idle.oneMinute")}
+                  </MenuItem>
+                  <MenuItem value={120}>
+                    {i18n.t("keyboardSettings.led.idle.twoMinutes")}
+                  </MenuItem>
+                  <MenuItem value={180}>
+                    {i18n.t("keyboardSettings.led.idle.threeMinutes")}
+                  </MenuItem>
+                  <MenuItem value={240}>
+                    {i18n.t("keyboardSettings.led.idle.fourMinutes")}
+                  </MenuItem>
+                  <MenuItem value={300}>
+                    {i18n.t("keyboardSettings.led.idle.fiveMinutes")}
+                  </MenuItem>
+                  <MenuItem value={600}>
+                    {i18n.t("keyboardSettings.led.idle.tenMinutes")}
+                  </MenuItem>
+                  <MenuItem value={900}>
+                    {i18n.t("keyboardSettings.led.idle.fifteenMinutes")}
+                  </MenuItem>
+                  <MenuItem value={1200}>
+                    {i18n.t("keyboardSettings.led.idle.twentyMinutes")}
+                  </MenuItem>
+                  <MenuItem value={1800}>
+                    {i18n.t("keyboardSettings.led.idle.thirtyMinutes")}
+                  </MenuItem>
+                  <MenuItem value={3600}>
+                    {i18n.t("keyboardSettings.led.idle.sixtyMinutes")}
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            )}
+            {ledBrightness >= 0 && (
+              <FormControl variant="standard" fullWidth={true}>
+                <Typography variant="caption" gutterBottom>
+                  {i18n.t("keyboardSettings.led.brightness")}
+                </Typography>
+                <Slider
+                  max={255}
+                  value={ledBrightness}
+                  sx={{ width: "300" }}
+                  onChange={setBrightness}
+                />
+              </FormControl>
+            )}
+          </FormGroup>
+        </CardContent>
+        <CardActions sx={{ flexGrow: 1 }}>
+          <SaveChangesButton onClick={saveKeymapChanges} disabled={!modified}>
+            {i18n.t("components.save.saveChanges")}
+          </SaveChangesButton>
+        </CardActions>
+      </Card>
+    </React.Fragment>
+  );
+};
+
+const AdvancedKeyboardSettings = () => {
+  const [EEPROMResetConfirmationOpen, setEEPROMResetConfirmationOpen] =
+    useState(false);
+  const [externalFlasherAvailable, setExternalFlasherAvailable] =
+    useState(false);
+  const [preferExternalFlasher, _setPreferExternalFlasher] = useState(false);
+  const [working, setWorking] = useState(false);
+  const resetEEPROM = async () => {
+    setWorking(true);
+    closeEEPROMResetConfirmation();
+
+    await clearEEPROM();
+    setWorking(false);
+  };
+  const openEEPROMResetConfirmation = () => {
+    setEEPROMResetConfirmationOpen(true);
+  };
+  const closeEEPROMResetConfirmation = () => {
+    setEEPROMResetConfirmationOpen(false);
+  };
+
+  const setPreferExternalFlasher = async (event) => {
+    settings.set("flash.preferExternalFlasher", event.target.checked);
+    _setPreferExternalFlasher(event.target.checked);
+  };
+
+  useEffect(() => {
+    const focus = new Focus();
+    checkExternalFlasher(focus.focusDeviceDescriptor).then(
+      async (available) => {
+        setExternalFlasherAvailable(available);
+        _setPreferExternalFlasher(
+          await settings.get("flash.preferExternalFlasher")
+        );
+      }
+    );
+  });
+
+  const preferExternalSwitch = (
+    <Switch
+      checked={preferExternalFlasher}
+      value="preferExternalFlasher"
+      onClick={setPreferExternalFlasher}
+    />
+  );
+
+  return (
+    <React.Fragment>
+      {working && <LinearProgress variant="query" />}
+      <Typography
+        variant="subtitle1"
+        component="h2"
+        sx={{
+          marginTop: 4,
+          marginBottom: 1,
+        }}
+      >
+        {i18n.t("keyboardSettings.advancedOps")}
+      </Typography>
+      <Card>
+        {externalFlasherAvailable && (
           <CardContent>
-            <FormGroup
+            <FormControl
               sx={{
                 display: "block",
               }}
             >
-              <FormControl variant="standard" fullWidth={true}>
-                <InputLabel>
-                  {i18n.t("keyboardSettings.keymap.defaultLayer")}
-                </InputLabel>
-                <Select
-                  onChange={this.selectDefaultLayer}
-                  sx={{ mb: 2 }}
-                  value={defaultLayer}
-                  variant="filled"
-                  input={<FilledInput sx={{}} />}
-                >
-                  <MenuItem value={126}>
-                    {i18n.t("keyboardSettings.keymap.noDefault")}
-                  </MenuItem>
-                  {layers}
-                </Select>
-              </FormControl>
-
-              {ledIdleTimeLimit >= 0 && (
-                <FormControl variant="standard" fullWidth={true}>
-                  <InputLabel>
-                    {i18n.t("keyboardSettings.led.idleTimeLimit")}
-                  </InputLabel>
-                  <Select
-                    onChange={this.selectIdleLEDTime}
-                    value={ledIdleTimeLimit}
-                    variant="filled"
-                    input={<FilledInput sx={{}} />}
-                  >
-                    <MenuItem value={0}>
-                      {i18n.t("keyboardSettings.led.idleDisabled")}
-                    </MenuItem>
-                    <MenuItem value={60}>
-                      {i18n.t("keyboardSettings.led.idle.oneMinute")}
-                    </MenuItem>
-                    <MenuItem value={120}>
-                      {i18n.t("keyboardSettings.led.idle.twoMinutes")}
-                    </MenuItem>
-                    <MenuItem value={180}>
-                      {i18n.t("keyboardSettings.led.idle.threeMinutes")}
-                    </MenuItem>
-                    <MenuItem value={240}>
-                      {i18n.t("keyboardSettings.led.idle.fourMinutes")}
-                    </MenuItem>
-                    <MenuItem value={300}>
-                      {i18n.t("keyboardSettings.led.idle.fiveMinutes")}
-                    </MenuItem>
-                    <MenuItem value={600}>
-                      {i18n.t("keyboardSettings.led.idle.tenMinutes")}
-                    </MenuItem>
-                    <MenuItem value={900}>
-                      {i18n.t("keyboardSettings.led.idle.fifteenMinutes")}
-                    </MenuItem>
-                    <MenuItem value={1200}>
-                      {i18n.t("keyboardSettings.led.idle.twentyMinutes")}
-                    </MenuItem>
-                    <MenuItem value={1800}>
-                      {i18n.t("keyboardSettings.led.idle.thirtyMinutes")}
-                    </MenuItem>
-                    <MenuItem value={3600}>
-                      {i18n.t("keyboardSettings.led.idle.sixtyMinutes")}
-                    </MenuItem>
-                  </Select>
-                </FormControl>
-              )}
-              {ledBrightness >= 0 && (
-                <FormControl variant="standard" fullWidth={true}>
-                  <Typography variant="caption" gutterBottom>
-                    {i18n.t("keyboardSettings.led.brightness")}
-                  </Typography>
-                  <Slider
-                    max={255}
-                    value={ledBrightness}
-                    sx={{ width: "300" }}
-                    onChange={this.setBrightness}
-                  />
-                </FormControl>
-              )}
-            </FormGroup>
-          </CardContent>
-          <CardActions sx={{ flexGrow: 1 }}>
-            <SaveChangesButton
-              onClick={this.saveKeymapChanges}
-              disabled={!modified}
-            >
-              {i18n.t("components.save.saveChanges")}
-            </SaveChangesButton>
-          </CardActions>
-        </Card>
-      </React.Fragment>
-    );
-  }
-}
-
-class AdvancedKeyboardSettings extends React.Component {
-  state = {
-    EEPROMResetConfirmationOpen: false,
-    externalFlasherAvailable: false,
-    preferExternalFlasher: false,
-  };
-
-  resetEEPROM = async () => {
-    await this.setState({ working: true });
-    this.closeEEPROMResetConfirmation();
-
-    await clearEEPROM();
-    this.setState({ working: false });
-  };
-  openEEPROMResetConfirmation = () => {
-    this.setState({ EEPROMResetConfirmationOpen: true });
-  };
-  closeEEPROMResetConfirmation = () => {
-    this.setState({ EEPROMResetConfirmationOpen: false });
-  };
-
-  setPreferExternalFlasher = async (event) => {
-    await settings.set("flash.preferExternalFlasher", event.target.checked);
-    this.setState({
-      preferExternalFlasher: event.target.checked,
-    });
-  };
-
-  componentDidMount() {
-    const focus = new Focus();
-    checkExternalFlasher(focus.focusDeviceDescriptor).then(
-      async (available) => {
-        this.setState({
-          externalFlasherAvailable: available,
-          preferExternalFlasher: await settings.get(
-            "flash.preferExternalFlasher"
-          ),
-        });
-      }
-    );
-  }
-
-  render() {
-    const { externalFlasherAvailable, preferExternalFlasher } = this.state;
-
-    const preferExternalSwitch = (
-      <Switch
-        checked={preferExternalFlasher}
-        value="preferExternalFlasher"
-        onClick={this.setPreferExternalFlasher}
-      />
-    );
-
-    return (
-      <React.Fragment>
-        {this.state.working && <LinearProgress variant="query" />}
-        <Typography
-          variant="subtitle1"
-          component="h2"
-          sx={{
-            marginTop: 4,
-            marginBottom: 1,
-          }}
-        >
-          {i18n.t("keyboardSettings.advancedOps")}
-        </Typography>
-        <Card>
-          {externalFlasherAvailable && (
-            <CardContent>
-              <FormControl
+              <FormControlLabel
                 sx={{
-                  display: "block",
+                  display: "flex",
+                  marginRight: 2,
                 }}
-              >
-                <FormControlLabel
-                  sx={{
-                    display: "flex",
-                    marginRight: 2,
-                  }}
-                  control={preferExternalSwitch}
-                  labelPlacement="start"
-                  label={i18n.t("keyboardSettings.flash.preferExternal")}
-                />
-              </FormControl>
-            </CardContent>
-          )}
-          <CardActions>
-            <Button
-              disabled={this.state.working}
-              variant="contained"
-              color="secondary"
-              onClick={this.openEEPROMResetConfirmation}
-            >
-              {i18n.t("keyboardSettings.resetEEPROM.button")}
-            </Button>
-          </CardActions>
-        </Card>
-        <ConfirmationDialog
-          title={i18n.t("keyboardSettings.resetEEPROM.dialogTitle")}
-          open={this.state.EEPROMResetConfirmationOpen}
-          onConfirm={this.resetEEPROM}
-          onCancel={this.closeEEPROMResetConfirmation}
-        >
-          {i18n.t("keyboardSettings.resetEEPROM.dialogContents")}
-        </ConfirmationDialog>
-      </React.Fragment>
-    );
-  }
-}
+                control={preferExternalSwitch}
+                labelPlacement="start"
+                label={i18n.t("keyboardSettings.flash.preferExternal")}
+              />
+            </FormControl>
+          </CardContent>
+        )}
+        <CardActions>
+          <Button
+            disabled={working}
+            variant="contained"
+            color="secondary"
+            onClick={openEEPROMResetConfirmation}
+          >
+            {i18n.t("keyboardSettings.resetEEPROM.button")}
+          </Button>
+        </CardActions>
+      </Card>
+      <ConfirmationDialog
+        title={i18n.t("keyboardSettings.resetEEPROM.dialogTitle")}
+        open={EEPROMResetConfirmationOpen}
+        onConfirm={resetEEPROM}
+        onCancel={closeEEPROMResetConfirmation}
+      >
+        {i18n.t("keyboardSettings.resetEEPROM.dialogContents")}
+      </ConfirmationDialog>
+    </React.Fragment>
+  );
+};
+
 export { KeyboardSettings, AdvancedKeyboardSettings };

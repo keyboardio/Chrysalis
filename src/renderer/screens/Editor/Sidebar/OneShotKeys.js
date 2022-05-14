54322b18b17bf134890d9fdb2db0c7ccfef8e388
Jesse Vincent <jesse@keyboard.io>
2022-05-13 22:16:37 -0700

    convert OneShotKeys to functional style

--- src/renderer/screens/Editor/Sidebar/OneShotKeys.js
+++ src/renderer/screens/Editor/Sidebar/OneShotKeys.js
@@ -14,8 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import i18n from "i18next";
 
 import Box from "@mui/material/Box";
@@ -23,7 +22,6 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormHelperText from "@mui/material/FormHelperText";
-import InputLabel from "@mui/material/InputLabel";
 import Switch from "@mui/material/Switch";
 
 import Collapsible from "../components/Collapsible";
@@ -36,82 +34,74 @@ const db = new KeymapDB();
 
 const cancelKeyCode = 53630;
 
-class OneShotKeys extends React.Component {
-  state = {
-    escCancel: true,
-  };
-
-  async componentDidMount() {
+const OneShotKeys = (props) => {
+  const [escCancel, setEscCancel] = useState(true);
+  useEffect(() => {
     const focus = new Focus();
-
-    let escCancel = await focus.command("escape_oneshot.cancel_key");
-    if (escCancel.length == 0) {
-      escCancel = false;
-    } else {
-      escCancel = parseInt(escCancel) == cancelKeyCode;
-    }
-
-    this.setState({
-      escCacnel: escCancel,
-    });
-  }
-
-  render() {
-    const { classes, keymap, selectedKey, layer, onKeyChange } = this.props;
-    const key = keymap.custom[layer][selectedKey];
-
-    const toggleEscapeCancel = async (event) => {
-      const focus = new Focus();
-      const escCancel = event.target.checked;
-      let newCancelKeyCode = cancelKeyCode;
-
-      if (escCancel) {
-        newCancelKeyCode = 41; // Esc
+    focus.command("escape_oneshot.cancel_key").then((escCancel) => {
+      if (escCancel.length == 0) {
+        escCancel = false;
+      } else {
+        escCancel = parseInt(escCancel) == cancelKeyCode;
       }
 
-      await focus.command("escape_oneshot.cancel_key", newCancelKeyCode);
-      this.setState({ escCancel: escCancel });
-    };
+      setEscCancel(escCancel);
+    });
+  }, []);
 
-    const escCancelWidget = (
-      <Switch
-        checked={this.state.escCancel}
-        color="primary"
-        onChange={toggleEscapeCancel}
-      />
-    );
+  const { classes, keymap, selectedKey, layer, onKeyChange } = props;
+  const key = keymap.custom[layer][selectedKey];
 
-    return (
-      <React.Fragment>
-        <Collapsible
-          expanded={key.code == cancelKeyCode || key.code == 41}
-          title={i18n.t("editor.sidebar.oneshot.title")}
-          help={i18n.t("editor.sidebar.oneshot.help")}
+  const toggleEscapeCancel = async (event) => {
+    const focus = new Focus();
+    const escCancel = event.target.checked;
+    let newCancelKeyCode = cancelKeyCode;
+
+    if (escCancel) {
+      newCancelKeyCode = 41; // Esc
+    }
+
+    await focus.command("escape_oneshot.cancel_key", newCancelKeyCode);
+    setEscCancel(escCancel);
+  };
+
+  const escCancelWidget = (
+    <Switch checked={escCancel} color="primary" onChange={toggleEscapeCancel} />
+  );
+  return (
+    <React.Fragment>
+      <Collapsible
+        expanded={key.code == cancelKeyCode || key.code == 41}
+        title={i18n.t("editor.sidebar.oneshot.title")}
+        help={i18n.t("editor.sidebar.oneshot.help")}
+      >
+        <KeyButton
+          keyObj={db.lookup(cancelKeyCode)}
+          onKeyChange={onKeyChange}
+        />
+
+        <Box
+          sx={{
+            m: "2 0",
+          }}
         >
-          <KeyButton
-            keyObj={db.lookup(cancelKeyCode)}
-            onKeyChange={onKeyChange}
-          />
-
-          <Box sx={{ m: "2 0" }}>
-            <FormControl component="fieldset">
-              <FormGroup row>
-                <FormControlLabel
-                  control={escCancelWidget}
-                  label={i18n.t(
-                    "editor.sidebar.oneshot.configuration.escCancelLabel"
-                  )}
-                />
-              </FormGroup>
-              <FormHelperText>
-                {i18n.t("editor.sidebar.oneshot.configuration.help")}
-              </FormHelperText>
-            </FormControl>
-          </Box>
-        </Collapsible>
-      </React.Fragment>
-    );
-  }
-}
+          <FormControl component="fieldset">
+            <FormGroup row>
+              <FormControlLabel
+                control={escCancelWidget}
+                label={i18n.t(
+                  "editor.sidebar.oneshot.configuration.escCancelLabel"
+                )}
+              />
+            </FormGroup>
+            <FormHelperText>
+              {i18n.t("editor.sidebar.oneshot.configuration.help")}
+            </FormHelperText>
+          </FormControl>
+        </Box>
+      </Collapsible>
+    </React.Fragment>
+  );
+};
 
 export { OneShotKeys as default };

669bfbd85e4111194e958bdf357746ac0799c340
Jesse Vincent <jesse@keyboard.io>
2022-05-16 18:25:28 -0700

    Convert collapsible to a functional component

--- src/renderer/screens/Editor/components/Collapsible.js
+++ src/renderer/screens/Editor/components/Collapsible.js
@@ -1,4 +1,5 @@
 // -*- mode: js-jsx -*-
+
 /* Chrysalis -- Kaleidoscope Command Center
  * Copyright (C) 2020  Keyboardio, Inc.
  *
@@ -14,8 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-import React from "react";
+import React, { useState } from "react";
 
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
@@ -25,76 +25,71 @@ import FormHelperText from "@mui/material/FormHelperText";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 
-class Collapsible extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      expanded: props.expanded === undefined ? true : props.expanded,
-    };
-  }
-
-  handleChange = () => {
-    return this.setState((oldState) => ({
-      expanded: !oldState.expanded,
-    }));
+const Collapsible = (props) => {
+  const [expanded, setExpanded] = useState(props.expanded);
+  const handleChange = () => {
+    setExpanded(!expanded);
   };
+  const { title, help } = props;
+  let summary = (
+    <AccordionSummary
+      expandIcon={<ExpandMoreIcon />}
+      sx={{
+        backgroundColor: "rgba(0, 0, 0, .03)",
+        border: "1px solid rgba(0, 0, 0, .125)",
+        padding: "0 2",
+        "&.Mui-expanded": {
+          margin: "0 0",
+          minHeight: "0",
+        },
+      }}
+    >
+      <Typography>{title}</Typography>
+    </AccordionSummary>
+  );
 
-  render() {
-    const { title, help } = this.props;
-    const { expanded } = this.state;
-
-    let summary = (
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        sx={{
-          backgroundColor: "rgba(0, 0, 0, .03)",
-          border: "1px solid rgba(0, 0, 0, .125)",
-          padding: "0 2",
-          "&.Mui-expanded": {
-            margin: "0 0",
-            minHeight: "0",
-          },
-        }}
-      >
-        <Typography>{title}</Typography>
-      </AccordionSummary>
-    );
-    if (help) {
-      summary = <Tooltip title={help}>{summary}</Tooltip>;
-    }
-
-    return (
-      <Accordion
-        square
-        expanded={expanded}
-        sx={{
-          boxShadow: "none",
-          margin: `0px 0px -1px 0px`,
-          "&.Mui-expanded": {
-            margin: "0 0",
-            minHeight: 48,
-          },
-          "&:before": {
-            display: "none",
-          },
-        }}
-        onChange={this.handleChange}
-      >
-        {summary}
-        <AccordionDetails
-          sx={{
-            padding: 2,
-            margin: "2 0",
-            display: "block",
-          }}
-        >
-          {help && <FormHelperText sx={{ mb: 2 }}>{help}</FormHelperText>}
-          {this.props.children}
-        </AccordionDetails>
-      </Accordion>
-    );
+  if (help) {
+    summary = <Tooltip title={help}>{summary}</Tooltip>;
   }
-}
+
+  return (
+    <Accordion
+      square
+      expanded={expanded}
+      sx={{
+        boxShadow: "none",
+        margin: `0px 0px -1px 0px`,
+        "&.Mui-expanded": {
+          margin: "0 0",
+          minHeight: 48,
+        },
+        "&:before": {
+          display: "none",
+        },
+      }}
+      onChange={handleChange}
+    >
+      {summary}
+      <AccordionDetails
+        sx={{
+          padding: 2,
+          margin: "2 0",
+          display: "block",
+        }}
+      >
+        {help && (
+          <FormHelperText
+            sx={{
+              mb: 2,
+            }}
+          >
+            {help}
+          </FormHelperText>
+        )}
+        {props.children}
+      </AccordionDetails>
+    </Accordion>
+  );
+};
 
 export { Collapsible as default };

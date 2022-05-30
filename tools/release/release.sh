7345fcc0e7b863af3364d2ad6f452f58618645e9
Gergely Nagy <algernon@keyboard.io>
2022-05-30 10:12:50 +0200

    tools/release/release.sh: Remove the unused extract_news() function
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- tools/release/release.sh
+++ tools/release/release.sh
@@ -139,15 +139,6 @@ EOF
     rm -f "${TMP}"
 }
 
-extract_news() {
-    PREV_RELEASE_LINE=$(expr $(grep -n "^===" NEWS.md | head -n 2 | tail -n 1 | cut -d: -f1) - 3)
-    if [ ${PREV_RELEASE_LINE} == 2 ]; then
-        PREV_RELEASE_LINE=$(wc -l NEWS.md)
-    fi
-
-    head -n ${PREV_RELEASE_LINE} NEWS.md | tail -n +5 | awk '{printf "%s ",$0} /^$/{print "\n"} END{print ""}'
-}
-
 push_changes() {
     git push
 }

7e0c140b24db483f947a1b37573d5237ace17188
Gergely Nagy <algernon@keyboard.io>
2022-09-26 00:13:01 +0200

    Implement a Breaking News component
    The component is meant to be placed just below the application bar, on a screen
    where it makes sense. It displays its children wrapped in an alert, and allows
    the user to permanently dismiss the news item.
    
    The component saves dismissals in a key among its settings, so future breaking
    news will still be shown.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

---
+++ src/renderer/components/BreakingNews.js
@@ -0,0 +1,39 @@
+// -*- mode: js-jsx -*-
+/* Chrysalis -- Kaleidoscope Command Center
+ * Copyright (C) 2022  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Alert from "@mui/material/Alert";
+import React from "react";
+
+const Store = require("electron-store");
+const settings = new Store();
+
+export const BreakingNews = (props) => {
+  const { tag, children } = props;
+
+  const seen = settings.get(`breaking-news.seen.${tag}`, false);
+  if (seen) return null;
+
+  const onClose = () => {
+    settings.set(`breaking-news.seen.${tag}`, true);
+  };
+
+  return (
+    <Alert severity="warning" onClose={onClose}>
+      {children}
+    </Alert>
+  );
+};

b85f8e17144e80b7461302c4467c7fe6cf82486a
Gergely Nagy <algernon@keyboard.io>
2022-10-01 14:05:20 +0200

    toolbox: make eslint & Tooltip happy about EraserIcon
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/renderer/screens/Editor/ToolBox.js
+++ src/renderer/screens/Editor/ToolBox.js
@@ -26,13 +26,12 @@ import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 // Sourced from: https://materialdesignicons.com/icon/eraser
-const EraserIcon = (props) => {
-  return (
-    <SvgIcon {...props}>
-      <path d="M16.24,3.56L21.19,8.5C21.97,9.29 21.97,10.55 21.19,11.34L12,20.53C10.44,22.09 7.91,22.09 6.34,20.53L2.81,17C2.03,16.21 2.03,14.95 2.81,14.16L13.41,3.56C14.2,2.78 15.46,2.78 16.24,3.56M4.22,15.58L7.76,19.11C8.54,19.9 9.8,19.9 10.59,19.11L14.12,15.58L9.17,10.63L4.22,15.58Z" />
-    </SvgIcon>
-  );
-};
+const EraserIcon = React.forwardRef((props, ref) => (
+  <SvgIcon {...props} ref={ref}>
+    <path d="M16.24,3.56L21.19,8.5C21.97,9.29 21.97,10.55 21.19,11.34L12,20.53C10.44,22.09 7.91,22.09 6.34,20.53L2.81,17C2.03,16.21 2.03,14.95 2.81,14.16L13.41,3.56C14.2,2.78 15.46,2.78 16.24,3.56M4.22,15.58L7.76,19.11C8.54,19.9 9.8,19.9 10.59,19.11L14.12,15.58L9.17,10.63L4.22,15.58Z" />
+  </SvgIcon>
+));
+EraserIcon.displayName = "EraserIcon";
 
 export const ToolBox = (props) => {
   const { t } = useTranslation();

67d434a21c900de8c534f46e4766d05547642bed
Jesse Vincent <jesse@keyboard.io>
2022-10-01 21:48:11 -0700

    We've got a nice dynamic language. Let's generate our accessors from data.

--- src/renderer/ActiveDevice.js
+++ src/renderer/ActiveDevice.js
@@ -101,25 +101,22 @@ export function ActiveDevice() {
     return cloneDeep(this._cache[command]);
   };
 
-  this.defaultLayer = async (newValue) => {
-    return await this._cachedDeviceData("settings.defaultLayer", newValue);
+  const cacheableFocusCommands = {
+    defaultLayer: "settings.defaultLayer",
+    keymap: "keymap",
+    colormap: "colormap",
+    macros: "macros",
+    layernames: "layernames",
   };
 
-  this.keymap = async (newValue) => {
-    return await this._cachedDeviceData("keymap", newValue);
-  };
-
-  this.colormap = async (newValue) => {
-    return await this._cachedDeviceData("colormap", newValue);
-  };
-
-  this.macros = async (newValue) => {
-    return await this._cachedDeviceData("macros", newValue);
-  };
-
-  this.layernames = async (newValue) => {
-    return await this._cachedDeviceData("layernames", newValue);
-  };
+  Object.keys(cacheableFocusCommands).forEach((command) => {
+    this[command] = async (newValue) => {
+      return await this._cachedDeviceData(
+        cacheableFocusCommands[command],
+        newValue
+      );
+    };
+  });
 
   this.version = async () => {
     if (this._storage.version === undefined) {

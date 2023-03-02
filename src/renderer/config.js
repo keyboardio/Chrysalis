96114d5f6705a56c42ef5495aa64fa3cebe087be
Jesse Vincent <jesse@keyboard.io>
2023-03-01 20:57:16 -0800

    Replace the old 'getStatic' method we only used in one place

--- src/renderer/config.js
+++ src/renderer/config.js
@@ -15,22 +15,19 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// TODO: const isDevelopment = process.env.NODE_ENV !== "production";
-const isDevelopment = true;
-
 import path from "path";
 import { ipcRenderer } from "electron";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 function getStaticPath() {
-  if (process.env.NODE_ENV !== "production") {
-    // The `__static` global doesn't match what it should in development.
-    // Instead, it includes an unexpected `node_modules` path, specifically:
-    // node_modules/electron/dist/Electron.app/Contents/Resources/static
-    return path.join(path.resolve(__dirname), "..", "..", "static");
+  // The `__static` global doesn't match what it should in development.
+  // Instead, it includes an unexpected `node_modules` path, specifically:
+  // node_modules/electron/dist/Electron.app/Contents/Resources/static
+  if (isDevelopment) {
+    return path.join("/static");
   } else {
-    return __static;
+    return path.join("../../../../static");
   }
 }
 
@@ -44,7 +41,6 @@ function getFilesystemPathForStaticAsset(asset) {
   } else {
     return path.join(__dirname, getStaticPath(), asset);
   }
-  return path.join(__static, p);
 }
 
 export { isDevelopment, getStaticPath, getFilesystemPathForStaticAsset };

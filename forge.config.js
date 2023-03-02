e81d6a75bb44bd49edddd2320ef3183831e9869d
Jesse Vincent <jesse@keyboard.io>
2023-03-02 13:50:04 -0800

    don't build rpms and debs

--- forge.config.js
+++ forge.config.js
@@ -13,15 +13,6 @@ module.exports = {
     },
     {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
-    },
-    {
-      name: "@electron-forge/maker-deb",
-      config: {},
-    },
-    {
-      name: "@electron-forge/maker-rpm",
-      config: {},
     },
   ],
    "publishers": [

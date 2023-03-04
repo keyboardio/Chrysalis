1f55ead04d4f8e8ffaf86932371936cd9a5f7826
Jesse Vincent <jesse@keyboard.io>
2023-03-03 22:03:13 -0800

    Notarize and sign release builds

--- forge.config.js
+++ forge.config.js
@@ -5,6 +5,22 @@ module.exports = {
   packagerConfig: {
     asar: true,
     extraResource: ["./build/launcher.sh", "static", "NEWS.md"],
+          osxSign: {
+			      optionsForFile: (filePath) => {       
+			        // Here, we keep it simple and return a single entitlements.plist file.
+			        // You can use this callback to map different sets of entitlements
+			        // to specific files in your packaged app.
+			        return {
+			          entitlements: './build/entitlements.mac.inherit.plist'
+					    }
+            }
+          },
+          osxNotarize: {
+            tool: 'notarytool',
+            appleId: process.env.APPLE_ID,
+            appleIdPassword: process.env.APPLE_PASSWORD,
+            teamId: process.env.APPLE_TEAM_ID
+          }
   },
   rebuildConfig: {},
   makers: [

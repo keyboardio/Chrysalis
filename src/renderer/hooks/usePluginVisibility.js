f211fe228d76bd499fe57871403cbaca26061720
Jesse Vincent <jesse@keyboard.io>
2023-03-21 13:50:33 -0700

    If the 'plugins' command isn't available, don't just enable all features

--- src/renderer/hooks/usePluginVisibility.js
+++ src/renderer/hooks/usePluginVisibility.js
@@ -39,7 +39,7 @@ export default function usePluginVisibility(plugin) {
           !hideUnavailableFeatures || plugins.includes(plugin)
         );
       } else {
-        setPluginSupported(true);
+        setPluginSupported(!hideUnavailableFeatures);
       }
     };
     fetchData();

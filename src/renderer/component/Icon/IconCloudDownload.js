949704ffaa907ee6aa305ce72f85a3641ad51bd4
Thiago Hernandez <thiago.hernandez@gmail.com>
2023-04-19 10:23:03 +0200

    Fixed cloud icon mask on SelectKeyboard view

--- src/renderer/component/Icon/IconCloudDownload.js
+++ src/renderer/component/Icon/IconCloudDownload.js
@@ -1,14 +1,25 @@
 import * as React from "react";
+
 function IconCloudDownload(props) {
   return (
     <svg width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <g clipPath="url(#prefix__clip0_3471_249004)">
-        <mask id="prefix__a" style={{ maskType: "alpha" }} maskUnits="userSpaceOnUse" x={0} y={0} width={24} height={24}>
+      <g clipPath="url(#prefix__clip0_3623_221104)">
+        <mask
+          id="prefix__aIconCloudDownload"
+          style={{
+            maskType: "alpha"
+          }}
+          maskUnits="userSpaceOnUse"
+          x={0}
+          y={0}
+          width={24}
+          height={24}
+        >
           <path d="M8 16v8H0V0h24v24h-8v-8H8z" fill="#fff" />
         </mask>
-        <g mask="url(#prefix__a)">
+        <g mask="url(#prefix__aIconCloudDownload)">
           <path
-            d="M15.664 8.956l-.225-.557.225.557A4.9 4.9 0 1117.5 18.4h-9a6.9 6.9 0 116.263-9.8c.158.34.555.495.9.356z"
+            d="M8.5 18.4a6.9 6.9 0 116.263-9.8c.158.34.555.495.9.356l-.224-.557.225.557A4.9 4.9 0 1117.5 18.4h-9z"
             stroke="currentColor"
             strokeWidth={1.2}
           />
@@ -17,11 +28,12 @@ function IconCloudDownload(props) {
         <path d="M10.57 20.65h2.86L12 22.08l-1.43-1.43z" stroke="currentColor" strokeWidth={1.3} />
       </g>
       <defs>
-        <clipPath id="prefix__clip0_3471_249004">
+        <clipPath id="prefix__clip0_3623_221104">
           <path fill="#fff" d="M0 0h24v24H0z" />
         </clipPath>
       </defs>
     </svg>
   );
 }
+
 export default IconCloudDownload;

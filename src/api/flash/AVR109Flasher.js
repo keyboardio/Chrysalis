dff9ee54843d9fd87b19f63ace69240d7e063929
Jesse Vincent <jesse@fsck.com>
2023-08-23 18:34:45 -0700

    Import avr109-webserial by Benjamin Aigner <aignerb@technikum-wien.at> Code as of 0aaf0a524.
    Original code was MIT licensed.

--- src/api/flash/AVR109Flasher.js
+++ src/api/flash/AVR109Flasher.js
@@ -84,4 +84,341 @@ const flash = async (board, port, filename, options) => {
   });
 };
 
+const fromHexString = (hexString) =>
+  new Uint8Array(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+//  <!-- Intel HEX parser by https://github.com/bminer/intel-hex.js -->
+//Intel Hex record types
+const DATA = 0,
+  END_OF_FILE = 1,
+  EXT_SEGMENT_ADDR = 2,
+  START_SEGMENT_ADDR = 3,
+  EXT_LINEAR_ADDR = 4,
+  START_LINEAR_ADDR = 5;
+
+const EMPTY_VALUE = 0xff;
+
+/* intel_hex.parse(data)
+        `data` - Intel Hex file (string in ASCII format or Buffer Object)
+        `bufferSize` - the size of the Buffer containing the data (optional)
+
+        returns an Object with the following properties:
+                - data - data as a Buffer Object, padded with 0xFF
+                        where data is empty.
+                - startSegmentAddress - the address provided by the last
+                        start segment address record; null, if not given
+                - startLinearAddress - the address provided by the last
+                        start linear address record; null, if not given
+        Special thanks to: http://en.wikipedia.org/wiki/Intel_HEX
+*/
+function parseIntelHex(data) {
+  //if(data instanceof Buffer)
+  data = data.toString("ascii");
+  //Initialization
+  var buf = new Uint8Array(32768); //max. words in mega32u4
+  var bufLength = 0, //Length of data in the buffer
+    highAddress = 0, //upper address
+    startSegmentAddress = null,
+    startLinearAddress = null,
+    lineNum = 0, //Line number in the Intel Hex string
+    pos = 0; //Current position in the Intel Hex string
+  const SMALLEST_LINE = 11;
+  while (pos + SMALLEST_LINE <= data.length) {
+    //Parse an entire line
+    if (data.charAt(pos++) != ":")
+      throw new Error(
+        "Line " + (lineNum + 1) + " does not start with a colon (:)."
+      );
+    else lineNum++;
+    //Number of bytes (hex digit pairs) in the data field
+    var dataLength = parseInt(data.substr(pos, 2), 16);
+    pos += 2;
+    //Get 16-bit address (big-endian)
+    var lowAddress = parseInt(data.substr(pos, 4), 16);
+    pos += 4;
+    //Record type
+    var recordType = parseInt(data.substr(pos, 2), 16);
+    pos += 2;
+    //Data field (hex-encoded string)
+    var dataField = data.substr(pos, dataLength * 2);
+    if (dataLength) var dataFieldBuf = fromHexString(dataField);
+    else var dataFieldBuf = new Uint8Array();
+    pos += dataLength * 2;
+    //Checksum
+    var checksum = parseInt(data.substr(pos, 2), 16);
+    pos += 2;
+    //Validate checksum
+    var calcChecksum =
+      (dataLength + (lowAddress >> 8) + lowAddress + recordType) & 0xff;
+    for (var i = 0; i < dataLength; i++)
+      calcChecksum = (calcChecksum + dataFieldBuf[i]) & 0xff;
+    calcChecksum = (0x100 - calcChecksum) & 0xff;
+    if (checksum != calcChecksum)
+      throw new Error(
+        "Invalid checksum on line " +
+          lineNum +
+          ": got " +
+          checksum +
+          ", but expected " +
+          calcChecksum
+      );
+    //Parse the record based on its recordType
+    switch (recordType) {
+      case DATA:
+        var absoluteAddress = highAddress + lowAddress;
+        //Expand buf, if necessary
+        /*if(absoluteAddress + dataLength >= buf.length)
+                                {
+                                        var tmp = Buffer.alloc((absoluteAddress + dataLength) * 2);
+                                        buf.copy(tmp, 0, 0, bufLength);
+                                        buf = tmp;
+                                }*/
+        //Write over skipped bytes with EMPTY_VALUE
+        if (absoluteAddress > bufLength)
+          buf.fill(EMPTY_VALUE, bufLength, absoluteAddress);
+        //Write the dataFieldBuf to buf
+        //dataFieldBuf.copy(buf, absoluteAddress);
+        dataFieldBuf.forEach(function (val, index) {
+          buf[absoluteAddress + index] = val;
+        });
+        bufLength = Math.max(bufLength, absoluteAddress + dataLength);
+        break;
+      case END_OF_FILE:
+        if (dataLength != 0)
+          throw new Error("Invalid EOF record on line " + lineNum + ".");
+        return {
+          data: buf.slice(0, bufLength),
+          startSegmentAddress: startSegmentAddress,
+          startLinearAddress: startLinearAddress,
+        };
+        break;
+      case EXT_SEGMENT_ADDR:
+        if (dataLength != 2 || lowAddress != 0)
+          throw new Error(
+            "Invalid extended segment address record on line " + lineNum + "."
+          );
+        highAddress = parseInt(dataField, 16) << 4;
+        break;
+      case START_SEGMENT_ADDR:
+        if (dataLength != 4 || lowAddress != 0)
+          throw new Error(
+            "Invalid start segment address record on line " + lineNum + "."
+          );
+        startSegmentAddress = parseInt(dataField, 16);
+        break;
+      case EXT_LINEAR_ADDR:
+        if (dataLength != 2 || lowAddress != 0)
+          throw new Error(
+            "Invalid extended linear address record on line " + lineNum + "."
+          );
+        highAddress = parseInt(dataField, 16) << 16;
+        break;
+      case START_LINEAR_ADDR:
+        if (dataLength != 4 || lowAddress != 0)
+          throw new Error(
+            "Invalid start linear address record on line " + lineNum + "."
+          );
+        startLinearAddress = parseInt(dataField, 16);
+        break;
+      default:
+        throw new Error(
+          "Invalid record type (" + recordType + ") on line " + lineNum
+        );
+        break;
+    }
+    //Advance to the next line
+    if (data.charAt(pos) == "\r") pos++;
+    if (data.charAt(pos) == "\n") pos++;
+  }
+  throw new Error("Unexpected end of input: missing or invalid EOF record.");
+}
+
+//credits: https://www.geeksforgeeks.org/how-to-delay-a-loop-in-javascript-using-async-await-with-promise/
+function waitforme(milisec) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("");
+    }, milisec);
+  });
+}
+
+//credits: https://www.30secondsofcode.org/articles/s/javascript-array-comparison
+const equals = (a, b) => a.length === b.length && a.every((v, i) => v === b[i]);
+
+/****************/
+// 1.) reset mega32u4 into bootloader mode (user interaction required)
+/****************/
+async function handleReset(e) {
+  if (!("serial" in navigator)) {
+    // The Web Serial API is not supported.
+    alert("Please use Chromium based browsers!");
+  }
+  e.preventDefault();
+  gear.classList.add("spinning");
+  let filters = [
+    //{ usbVendorId: 0x2341, usbProductId: 0x8037 }
+    //TODO: I think there are more possible PIDs...
+  ];
+  port = await navigator.serial.requestPort({ filters });
+
+  //open & close
+  // Wait for the serial port to open.
+  await port.open({ baudRate: 1200 });
+  await waitforme(500);
+  await port.close();
+  await waitforme(500);
+  gear.classList.remove("spinning");
+}
+
+/****************/
+// 2.) open the new bootloader USB-serial (new USB-PID!); user interaction required
+/****************/
+async function handleSubmit(e) {
+  if (!("serial" in navigator)) {
+    // The Web Serial API is not supported.
+    alert("Please use Chromium based browsers!");
+  }
+  e.preventDefault();
+
+  let filecontents;
+  const file = fileInput.files[0];
+  const readerF = new FileReader();
+
+  readerF.onload = async function (event) {
+    filecontents = event.target.result;
+    gear.classList.add("spinning");
+
+    //parse intel hex
+    let flashData = parseIntelHex(filecontents);
+
+    //request serial port
+    let filters = [
+      { usbVendorId: 0x2341, usbProductId: 0x0036 },
+      //TODO: I think there are more possible PIDs...
+    ];
+    port = await navigator.serial.requestPort({ filters });
+
+    //open & close
+    // Wait for the serial port to open.
+    await port.open({ baudRate: 57600 });
+
+    //open writing facilities (with text encoder -> not good!)
+    /*const textEncoder = new TextEncoderStream();
+                  const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+                  const writer = textEncoder.writable.getWriter();*/
+    //open writing facilities
+    const writer = port.writable.getWriter();
+    //open reading stream
+    const reader = port.readable.getReader();
+
+    //trigger update by sending programmer ID command
+    await writer.write(new Uint8Array([0x53]));
+
+    // Listen to data coming from the serial device.
+    let state = 0;
+    let pageStart = 0;
+    let address = 0;
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) {
+        // Allow the serial port to be closed later.
+        reader.releaseLock();
+        writer.releaseLock();
+        break;
+      }
+      // value is a Uint8Array.
+      console.log(value);
+
+      /****************/
+      // 3.) flashing the .hex file (event driven by the received data from the ATMega32U4).
+      // most commands are acknowledged with 13d.
+      /****************/
+      if (state == 0) {
+        //1.) "S" => "CATERIN" - get programmer ID
+        if (equals(value, [67, 65, 84, 69, 82, 73, 78])) {
+          console.log(
+            'programmer "CATERIN" detected, entering programming mode'
+          );
+          await writer.write(new Uint8Array([0x50]));
+          await waitforme(5);
+          state = 1;
+        } else {
+          console.log(
+            'error: unexpected RX value in state 0, waited for "CATERIN"'
+          );
+        }
+      } else if (state == 1) {
+        //2.) "P" => 13d - enter programming mode
+        if (equals(value, [13])) {
+          console.log("setting address to: " + address);
+          data = new Uint8Array([0x41, (address >> 8) & 0xff, address & 0xff]); // 'A' high low
+          console.log("O: " + data);
+          await writer.write(data);
+          await waitforme(5);
+          state = 2;
+        } else {
+          console.log("error: unexpected RX value in state 1, waited for 13");
+        }
+      } else if (state == 2) {
+        //3.) now flash page
+        if (equals(value, [13])) {
+          cmd = new Uint8Array([0x42, 0x00, 0x80, 0x46]); //flash page write command ('B' + 2bytes size + 'F')
+
+          //determine if this is the last page (maybe incomplete -> fill with 0xFF)
+          if (pageStart + 128 > flashData.data.length) {
+            data = flashData.data.slice(pageStart); //take the remaining bit
+            pad = new Uint8Array(128 - data.length); //create a new padding array
+            pad.fill(0xff);
+            txx = Uint8Array.from([...cmd, ...data, ...pad]); //concat command, remaining data and padding
+            console.log("last page");
+            state = 3;
+          } else {
+            data = flashData.data.slice(pageStart, pageStart + 128); //take subarray with 128B
+            txx = Uint8Array.from([...cmd, ...data]); //concate command with page data
+            state = 1;
+          }
+
+          console.log("adress set, writing one page: " + data);
+          pageStart += 128;
+          address += 64;
+          //write control + flash data
+          await writer.write(txx);
+          console.log("O: " + txx);
+          await waitforme(5);
+        } else {
+          console.log("error: state 2");
+        }
+      } else if (state == 3) {
+        //4.) last page sent, finish update
+        if (value[0] == 13) {
+          console.log("Last page write, leaving programming mode");
+          //finish flashing and exit bootloader
+          await writer.write(new Uint8Array([0x4c])); //"L" -> leave programming mode
+          state = 4;
+          /*state = -1;
+                                          gear.classList.remove('spinning');
+                                          console.log("finished!");
+                                          reader.cancel();*/
+        } else {
+          console.log("NACK");
+        }
+      } else if (state == 4) {
+        //5.) left programming mode, exiting bootloader
+        if (value[0] == 13) {
+          console.log("Exiting bootloader");
+          //finish flashing and exit bootloader
+          await writer.write(new Uint8Array([0x45])); //"E" -> exit bootloader
+          state = -1;
+          gear.classList.remove("spinning");
+          console.log("finished!");
+          reader.cancel();
+        } else {
+          console.log("NACK");
+        }
+      }
+    }
+    await port.close();
+  };
+  readerF.readAsText(file);
+}
+
 export const AVR109Flasher = { flash, rebootToApplicationMode };

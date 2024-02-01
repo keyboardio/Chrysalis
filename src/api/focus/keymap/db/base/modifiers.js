c9beb057911759bb24f44521c5a93870b393d8be
Jesse Vincent <jesse@keyboard.io>
2024-01-31 18:12:29 -0800

    switch to a more scannable format for this database file

--- src/api/focus/keymap/db/base/modifiers.js
+++ src/api/focus/keymap/db/base/modifiers.js
@@ -13,100 +13,26 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import { GuiLabel } from "./gui";
 import addCategories from "../addCategories";
 import { withModifiers } from "../modifiers";
-
 const modifiers = withModifiers(
   addCategories(
     ["modifier"],
     [
-      {
-        code: 224,
-        label: {
-          base: {
-            full: "Control",
-            "1u": "Ctrl",
-          },
-        },
-        location: "left",
-      },
-      {
-        code: 225,
-        label: {
-          base: "Shift",
-        },
-        location: "left",
-      },
-      {
-        code: 226,
-        label: {
-          base: "Alt",
-        },
-        location: "left",
-      },
-      {
-        code: 227,
-        label: {
-          base: GuiLabel,
-        },
-        location: "left",
-      },
-      {
-        code: 228,
-        label: {
-          base: {
-            full: "Control",
-            "1u": "Ctrl",
-          },
-        },
-        location: "right",
-      },
-      {
-        code: 229,
-        label: {
-          base: "Shift",
-        },
-        location: "right",
-      },
-      {
-        code: 230,
-        label: {
-          base: {
-            full: "AltGr",
-            "1u": "AGr",
-          },
-        },
-        location: "right",
-      },
-      {
-        code: 231,
-        label: {
-          base: GuiLabel,
-        },
-        location: "right",
-      },
-    ]
-  )
+      { code: 224, label: { base: { full: "Control", "1u": "Ctrl" } }, location: "left" },
+      { code: 225, label: { base: "Shift" }, location: "left" },
+      { code: 226, label: { base: "Alt" }, location: "left" },
+      { code: 227, label: { base: GuiLabel }, location: "left" },
+      { code: 228, label: { base: { full: "Control", "1u": "Ctrl" } }, location: "right" },
+      { code: 229, label: { base: "Shift" }, location: "right" },
+      { code: 230, label: { base: { full: "AltGr", "1u": "AGr" } }, location: "right" },
+      { code: 231, label: { base: GuiLabel }, location: "right" },
+    ],
+  ),
 ).concat([
   // Custom modifier combos
-  {
-    code: 2530,
-    baseCode: 226,
-    categories: ["ctrl", "shift"],
-    label: {
-      base: "Meh",
-    },
-  },
-  {
-    code: 3043,
-    baseCode: 227,
-    categories: ["ctrl", "shift", "alt"],
-    label: {
-      base: "Hyper",
-    },
-  },
+  { code: 2530, baseCode: 226, categories: ["ctrl", "shift"], label: { base: "Meh" } },
+  { code: 3043, baseCode: 227, categories: ["ctrl", "shift", "alt"], label: { base: "Hyper" } },
 ]);
-
 export { modifiers };

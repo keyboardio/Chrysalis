9219d07852937c830dafc4120e32674e5afbb3e6
Jesse Vincent <jesse@keyboard.io>
2024-01-31 18:11:26 -0800

    switch to a more scannable format for this database file

--- src/api/focus/keymap/db/base/navigation.js
+++ src/api/focus/keymap/db/base/navigation.js
@@ -13,76 +13,15 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import { withModifiers } from "../modifiers";
-
 const navigation = withModifiers([
-  {
-    code: 74,
-    label: {
-      base: "Home",
-    },
-  },
-  {
-    code: 75,
-    label: {
-      base: {
-        full: "Page Up",
-        "1u": "PgUp",
-      },
-    },
-  },
-  {
-    code: 77,
-    label: {
-      base: "End",
-    },
-  },
-  {
-    code: 78,
-    label: {
-      base: {
-        full: "Page Down",
-        "1u": "PgDn",
-      },
-    },
-  },
-  {
-    code: 79,
-    label: {
-      base: {
-        full: "Right Arrow",
-        "1u": "→",
-      },
-    },
-  },
-  {
-    code: 80,
-    label: {
-      base: {
-        full: "Left Arrow",
-        "1u": "←",
-      },
-    },
-  },
-  {
-    code: 81,
-    label: {
-      base: {
-        full: "Down Arrow",
-        "1u": "↓",
-      },
-    },
-  },
-  {
-    code: 82,
-    label: {
-      base: {
-        full: "Up Arrow",
-        "1u": "↑",
-      },
-    },
-  },
+  { code: 74, label: { base: "Home" } },
+  { code: 75, label: { base: { full: "Page Up", "1u": "PgUp" } } },
+  { code: 77, label: { base: "End" } },
+  { code: 78, label: { base: { full: "Page Down", "1u": "PgDn" } } },
+  { code: 79, label: { base: { full: "Right Arrow", "1u": "→" } } },
+  { code: 80, label: { base: { full: "Left Arrow", "1u": "←" } } },
+  { code: 81, label: { base: { full: "Down Arrow", "1u": "↓" } } },
+  { code: 82, label: { base: { full: "Up Arrow", "1u": "↑" } } },
 ]);
-
 export { navigation };

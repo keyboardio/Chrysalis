bde344e4ef302d7eca7011c8925314f04a159d52
Jesse Vincent <jesse@keyboard.io>
2024-01-31 18:11:11 -0800

    switch to a more scannable format for this database file

--- src/api/focus/keymap/db/base/numpad.js
+++ src/api/focus/keymap/db/base/numpad.js
@@ -13,131 +13,24 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import { withModifiers } from "../modifiers";
-
 const numpad = withModifiers([
-  {
-    code: 83,
-    label: {
-      base: {
-        full: "Num Lock",
-        "1u": "Num",
-      },
-    },
-  },
-  {
-    code: 84,
-    label: {
-      base: "/",
-    },
-    location: "numpad",
-  },
-  {
-    code: 85,
-    label: {
-      base: "*",
-    },
-    location: "numpad",
-  },
-  {
-    code: 86,
-    label: {
-      base: "-",
-    },
-    location: "numpad",
-  },
-  {
-    code: 87,
-    label: {
-      base: "+",
-    },
-    location: "numpad",
-  },
-  {
-    code: 88,
-    label: {
-      base: { full: "Enter", "1u": "Ent" },
-    },
-    location: "numpad",
-  },
-  {
-    code: 89,
-    label: {
-      base: "1",
-    },
-    location: "numpad",
-  },
-  {
-    code: 90,
-    label: {
-      base: "2",
-    },
-    location: "numpad",
-  },
-  {
-    code: 91,
-    label: {
-      base: "3",
-    },
-    location: "numpad",
-  },
-  {
-    code: 92,
-    label: {
-      base: "4",
-    },
-    location: "numpad",
-  },
-  {
-    code: 93,
-    label: {
-      base: "5",
-    },
-    location: "numpad",
-  },
-  {
-    code: 94,
-    label: {
-      base: "6",
-    },
-    location: "numpad",
-  },
-  {
-    code: 95,
-    label: {
-      base: "7",
-    },
-    location: "numpad",
-  },
-  {
-    code: 96,
-    label: {
-      base: "8",
-    },
-    location: "numpad",
-  },
-  {
-    code: 97,
-    label: {
-      base: "9",
-    },
-    location: "numpad",
-  },
-  {
-    code: 98,
-    label: {
-      base: "0",
-    },
-    location: "numpad",
-  },
-  {
-    code: 99,
-    label: {
-      base: ".",
-    },
-    location: "numpad",
-  },
+  { code: 83, label: { base: { full: "Num Lock", "1u": "Num" } } },
+  { code: 84, label: { base: "/" }, location: "numpad" },
+  { code: 85, label: { base: "*" }, location: "numpad" },
+  { code: 86, label: { base: "-" }, location: "numpad" },
+  { code: 87, label: { base: "+" }, location: "numpad" },
+  { code: 88, label: { base: { full: "Enter", "1u": "Ent" } }, location: "numpad" },
+  { code: 89, label: { base: "1" }, location: "numpad" },
+  { code: 90, label: { base: "2" }, location: "numpad" },
+  { code: 91, label: { base: "3" }, location: "numpad" },
+  { code: 92, label: { base: "4" }, location: "numpad" },
+  { code: 93, label: { base: "5" }, location: "numpad" },
+  { code: 94, label: { base: "6" }, location: "numpad" },
+  { code: 95, label: { base: "7" }, location: "numpad" },
+  { code: 96, label: { base: "8" }, location: "numpad" },
+  { code: 97, label: { base: "9" }, location: "numpad" },
+  { code: 98, label: { base: "0" }, location: "numpad" },
+  { code: 99, label: { base: "." }, location: "numpad" },
 ]);
-
 export { numpad };

382c87a3dc1a6ddff35504fa419a4acd982b15f6
Jesse Vincent <jesse@keyboard.io>
2024-01-31 18:14:40 -0800

    switch to a more scannable format for this database file

--- src/api/focus/keymap/db/base/gui.js
+++ src/api/focus/keymap/db/base/gui.js
@@ -13,31 +13,12 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 const GuiLabels = {
-  linux: {
-    full: "Super",
-    "1u": "Sup.",
-    short: "Su",
-  },
-  win32: {
-    full: "Windows",
-    "1u": "Win",
-    short: "W",
-  },
-  darwin: {
-    full: "Command",
-    "1u": "Cmd",
-    short: "Cm",
-  },
-  default: {
-    full: "Gui",
-    "1u": "Gui",
-    short: "G",
-  },
+  linux: { full: "Super", "1u": "Sup.", short: "Su" },
+  win32: { full: "Windows", "1u": "Win", short: "W" },
+  darwin: { full: "Command", "1u": "Cmd", short: "Cm" },
+  default: { full: "Gui", "1u": "Gui", short: "G" },
 };
-
 const GuiLabel = GuiLabels.default; // TODO: GuiLabels[process.platform] || GuiLabels.default;
 const GuiShortLabel = GuiLabel.short;
-
 export { GuiLabel, GuiShortLabel };

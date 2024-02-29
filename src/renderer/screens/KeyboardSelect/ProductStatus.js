b65228d7b6b34f81cb023de3c65f821c761b6556
Jesse Vincent <jesse@keyboard.io>
2024-02-29 14:18:38 -0800

    Further dep reduction

--- src/renderer/screens/KeyboardSelect/ProductStatus.js
+++ src/renderer/screens/KeyboardSelect/ProductStatus.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { marked } from "marked";
+import Markdown from 'react-markdown'
 
 // Import the raw content of the markdown file
 import markdownFile from "@root/product-status.md";
 
 export const ProductStatus = () => {
   // Convert the raw markdown content to HTML
-  const htmlContent = marked(markdownFile);
-
-  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+  return <Markdown>{markdownFile}</Markdown>
 };

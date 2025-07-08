3fc049fc18428f5e38630557f97d90ca40393cfd
thehenkan <thehenkan@gmail.com>
2025-07-07 23:38:58 -0700

    Add backup/restore support for EEPROM settings
    This adds a button to dump EEPROM settings to a JSON file,
    and a button to restore them by uploading a backup.
    
    Signed-off-by: thehenkan <thehenkan@gmail.com>

--- src/renderer/screens/Editor/Sidebar/LayoutSharing/FileImport.js
+++ src/renderer/screens/Editor/Sidebar/LayoutSharing/FileImport.js
@@ -1,41 +1,21 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import logger from "@renderer/utils/Logger";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { loadLayout } from "./LoadLayout";
+import { FileInput } from "../../../../components/FileUpload";
 
 export const FileImport = (props) => {
   const { t } = useTranslation();
 
-  const fileInputRef = React.createRef();
-
-  const importFromFile = async (event) => {
-    const file = event.target.files[0];
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = async function (e) {
-      const fileData = e.target.result;
-      const layoutData = await loadLayout(file.name, fileData);
-      if (layoutData != null) props.onRestore(layoutData);
-      logger.log("finally returned ", layoutData);
-    };
-    reader.readAsText(file);
-  };
-
-  const openFileDialog = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
+  const loadFile = async (fileData, fileName) => {
+    const layoutData = await loadLayout(fileName, fileData);
+    if (layoutData != null) props.onRestore(layoutData);
+    logger.log("finally returned ", layoutData);
   };
 
   return (
-    <Box sx={{ mb: 2 }}>
-      <input type="file" style={{ display: "none" }} ref={fileInputRef} onChange={importFromFile} />
-      <Button variant="outlined" onClick={openFileDialog}>
-        {t("editor.sharing.loadFromFile")}
-      </Button>
-    </Box>
+    <FileInput onLoad={loadFile}>
+      {t("editor.sharing.loadFromFile")}
+    </FileInput>
   );
 };
